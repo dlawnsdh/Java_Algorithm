a094ac790c5265b3e8e625f02ddb32d3f263fa7a
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2023-02-19 15:30:19 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        String s = Integer.toString(n);
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++)
+            sum += s.charAt(i) - 48;
+        return sum;
+    }
+}
