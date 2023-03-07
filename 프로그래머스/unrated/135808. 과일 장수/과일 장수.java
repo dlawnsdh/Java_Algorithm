923b6b6a7eaa199e395707053d56f9f09e495913
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2023-03-07 16:57:50 +0900

    Update 과일 장수.java

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -4,10 +4,9 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int sum = 0;
         Arrays.sort(score);
-        for (int i =  score.length % m; i < score.length; i += m) {
-            int[] arr = Arrays.copyOfRange(score, i, i + m);
-            sum += arr[0] * m;
-        }
+        for (int i =  score.length % m; i < score.length; i += m) 
+            sum += score[i] * m;
+        
         return sum;
     }
 }
