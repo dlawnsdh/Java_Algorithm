3770bb2ac9b7b10b4fd5e57729d6e2b20ab1b709
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2023-06-07 15:19:30 +0900

    [level 1] Title: 체육복, Time: 0.40 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] arr = new int[n + 1];
+        Arrays.fill(arr, 1);
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        for (int i = 0; i < reserve.length; i++)
+            arr[reserve[i]]++;
+        for (int i = 0; i < lost.length; i++)
+            arr[lost[i]]--;
+                
+        for (int i = 0; i < lost.length; i++) 
+          Outer : for (int k = 0; k < reserve.length; k++) 
+                if (arr[reserve[k]] == 2 && arr[lost[i]] == 0) {
+                    if (lost[i] - 1 == reserve[k] || lost[i] + 1 == reserve[k]) {
+                        arr[reserve[k]]--;
+                        arr[lost[i]]++;
+                        break Outer;   
+                    }
+                }
+        
+        int cnt = -1;
+        for (int i : arr)
+            if (i > 0)
+                cnt++;
+        return cnt;
+    }
+}
