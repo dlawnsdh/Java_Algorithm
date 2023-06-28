f5e5ec33ca4644b2d436feb679b89e65a7fcd56d
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2023-06-28 20:46:51 +0900

    Update 에디터.java

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -4,44 +4,44 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String str = br.readLine();
-		int n = Integer.parseInt(br.readLine());
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	String str = br.readLine();
+	int n = Integer.parseInt(br.readLine());
 
-		Stack<Character> l = new Stack<>();
+	Stack<Character> l = new Stack<>();
         Stack<Character> r = new Stack<>();
         for (int i = 0; i < str.length(); i++)
             l.push(str.charAt(i));
 		
-		for(int i = 0; i < n; i++) {
-			String o = br.readLine();
-			char c = o.charAt(0);
+	for(int i = 0; i < n; i++) {
+	    String o = br.readLine();
+	    char c = o.charAt(0);
 			
-			switch(c) {
-			case 'L':
-				if (!l.isEmpty())
-                    r.push(l.pop());
-                break;
-			case 'D':
-				if(!r.isEmpty())
-					l.push(r.pop());
-				break;
-			case 'B':
-				if(!l.isEmpty()) 
-					l.pop();
-				break;
-			case 'P':
-				l.push(o.charAt(2));
-				break;
-			}
-		}
+	    switch(c) {
+		case 'L':
+		    if (!l.isEmpty())
+                    	r.push(l.pop());
+                    break;
+		case 'D':
+		    if(!r.isEmpty())
+			l.push(r.pop());
+		    break;
+		case 'B':
+		    if(!l.isEmpty()) 
+			l.pop();
+		    break;
+	       	case 'P':
+		    l.push(o.charAt(2));
+		    break;
+	    }
+	}
         
-		while(!l.isEmpty())
-			r.push(l.pop());
-		while(!r.isEmpty())
-			bw.write(r.pop());
+	while(!l.isEmpty())
+	    r.push(l.pop());
+	while(!r.isEmpty())
+	    bw.write(r.pop());
 		
-		bw.flush();
-		bw.close(); 
+	bw.flush();
+	bw.close(); 
     }
 }
