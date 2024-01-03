953bc0f5d260e964929da9b5db88f5fa217102c9
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-03 22:09:18 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,20 +1,13 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
         String str = s.nextLine();
+        
         String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-        int cnt = 0;
-
-        for (String string : arr) {
-            String tmp = str;
-            String str2 = str.replaceAll(string, "");
-            str = str.replaceAll(string, " ");
-            if (!str.equals(tmp))
-                cnt += str.length() - str2.length();
-        }
-        str = str.replaceAll(" ", "");
-        System.out.println(cnt + str.length());
-    }
+        for (String o : arr)
+            str = str.replaceAll(o, "@");
+        System.out.print(str.length());
+	}
 }
