63404bd8c6d93725fcfe1f1b055836339cdd761e
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-03 20:01:53 +0900

    [Silver V] Title: 셀프 넘버, Time: 172 ms, Memory: 15748 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = new int[10001];
 
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 1; i < 10001; i++) {
             String str = Integer.toString(i);
-            int sum = 0;
+            int sum = i;
             for (int k = 0; k < str.length(); k++)
                 sum += str.charAt(k) - 48;
-            if (i + sum <= 10000)
-                arr[i + sum]++;
+            if (sum < 10001)
+                arr[sum]++;
         }
-        
-        for (int i = 1; i < arr.length; i++)
+
+        for (int i = 1; i < 10001; i++)
             if (arr[i] == 0)
                 System.out.println(i);
     }
