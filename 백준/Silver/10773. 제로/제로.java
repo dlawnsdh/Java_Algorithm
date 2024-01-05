c2eaac6a1c97f3d1dfd11844d5b0fc618bcf69aa
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-05 14:42:48 +0900

    [Silver IV] Title: 제로, Time: 908 ms, Memory: 111052 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,18 +1,17 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+        int k = s.nextInt();
+        
         Stack<Integer> st = new Stack<>();
-        int n = s.nextInt();
-        
-        for (int i = 0; i < n; i++) {
-            int k = s.nextInt();
-            if (k == 0)
-                st.pop();
-            else st.push(k);
+        for (int i = 0; i < k; i++) {
+            int n = s.nextInt();
+            if (st.isEmpty() || n != 0)
+                st.push(n);
+            else st.pop();
         }
-        
-        System.out.print(st.stream().mapToInt(Integer::intValue).sum());
-    }
+        System.out.print(st.stream().reduce(Integer::sum).orElse(0));
+	}
 }
