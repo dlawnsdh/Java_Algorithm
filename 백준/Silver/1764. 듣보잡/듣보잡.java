341fc3bc85f4a0efbda1cefd7c2a197f7e91c496
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-07 14:41:22 +0900

    [Silver IV] Title: 듣보잡, Time: 1048 ms, Memory: 193860 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -7,12 +7,12 @@ public class Main {
         String[] arr = s.nextLine().split(" ");
         Set<String> s1 = new HashSet<>();
         Set<String> s2 = new HashSet<>();
-        
+
         for (int i = 0; i < Integer.parseInt(arr[0]); i++)
             s1.add(s.nextLine());
         for (int i = 0; i < Integer.parseInt(arr[1]); i++)
             s2.add(s.nextLine());    
-            
+
         List<String> result = s1.stream().filter(s2::contains).sorted().collect(Collectors.toList());
         System.out.println(result.size());
         result.forEach(System.out::println);
