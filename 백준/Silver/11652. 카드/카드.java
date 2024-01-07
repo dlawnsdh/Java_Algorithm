b393166401643cf1e89615a171413203974d0b24
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-07 17:42:10 +0900

    [Silver IV] Title: 카드, Time: 1292 ms, Memory: 126032 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        Map<Long, Integer> m = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            long k = s.nextLong();
+            if (!m.containsKey(k)) m.put(k, 1);
+            else m.put(k, m.get(k) + 1);
+        }
+        
+        int max = m.values().stream().max(Integer::compareTo).orElse(0);
+        long num = m.keySet().stream().filter(l -> m.get(l) == max).sorted().findFirst().orElse(0L);
+        System.out.print(num);
+    }
+}
