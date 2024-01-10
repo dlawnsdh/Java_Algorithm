f5474a55b4ff736dbe1089f3d0ff106506ff4a81
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-10 15:51:58 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 236 ms, Memory: 18328 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -3,12 +3,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int[] arr = new int[26];
         String str = s.nextLine();
+        int[] arr = new int[26];
         
         for (int i = 0; i < str.length(); i++)
             arr[str.charAt(i) - 97]++;
         
-        Arrays.stream(arr).forEach(i -> System.out.print(i + " "));
+        for (int i : arr)
+            System.out.print(i + " ");
     }
 }
