d2f7c5a7880dab52235791b935704cd23a1f4da3
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-10 17:11:22 +0900

    [Silver III] Title: 두 수의 합, Time: 1036 ms, Memory: 110052 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,21 +1,14 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int[] arr = new int[n];
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < n; i++)
-            arr[i] = s.nextInt();
+            set.add(s.nextInt());
         int x = s.nextInt();
-       
-        int cnt = 0;
-        for (int i = 0; i < arr.length - 1; i++)
-            for (int k = i + 1; k < arr.length; k++)
-                if (arr[i] + arr[k] == x) {
-                    cnt++;
-                    break;
-                }
-        System.out.print(cnt);
-    }
+        
+        System.out.print(set.stream().filter(i -> set.contains(x - i)).count() / 2);
+	}
 }
