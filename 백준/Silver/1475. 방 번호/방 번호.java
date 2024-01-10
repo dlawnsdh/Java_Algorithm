d93a10143e457885c6d584ce08368693299d4948
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-10 16:18:32 +0900

    [Silver V] Title: 방 번호, Time: 200 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -2,21 +2,17 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+	    Scanner s = new Scanner(System.in);
         String str = s.nextLine();
         int[] arr = new int[10];
-        
         for (int i = 0; i < str.length(); i++)
             arr[str.charAt(i) - 48]++;
         
-        int index = 0;
         int max = 0;
-        for (int i = 0; i < arr.length; i++)
-            if (i != 6 && i != 9) 
+        for (int i = 0; i < arr.length; i++) 
+            if (i != 6 && i != 9)
                 max = Math.max(max, arr[i]);
-        int sum = arr[6] + arr[9];    
-        max = Math.max(max, sum / 2 + sum % 2);
-       
-        System.out.print(max);
+        int k = (arr[6] + arr[9]) / 2 + (arr[6] + arr[9]) % 2;
+        System.out.print(Math.max(k, max));
     }
 }
