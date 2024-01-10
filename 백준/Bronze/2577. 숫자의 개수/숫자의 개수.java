9f2a41dfc1aa1b4eb82cbed1da80e322c27d5cc1
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-10 15:58:04 +0900

    [Bronze II] Title: 숫자의 개수, Time: 208 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -3,16 +3,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
-        int b = s.nextInt();
-        int c = s.nextInt();
-        
+        int a = s.nextInt(); int b = s.nextInt(); int c = s.nextInt();
         String str = Integer.toString(a * b * c);
-        int[] arr = new int[10];
+        int[] arr  = new int[10];
+        
         for (int i = 0; i < str.length(); i++)
             arr[str.charAt(i) - 48]++;
         
-        for (int i = 0; i < arr.length; i++)
-            System.out.println(arr[i]);
+        for (int i : arr) System.out.println(i);
     }
 }
