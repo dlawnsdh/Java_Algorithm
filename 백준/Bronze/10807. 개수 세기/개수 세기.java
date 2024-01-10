9aa43085489884d06f3cc86e523497506c41e5de
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-10 17:31:25 +0900

    [Bronze V] Title: 개수 세기, Time: 220 ms, Memory: 17872 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -7,14 +7,11 @@ public class Main {
         Map<Integer, Integer> m = new HashMap<>();
         for (int i = 0; i < n; i++) {
             int k = s.nextInt();
-            if (!m.containsKey(k))
-                m.put(k, 1);
+            if (!m.containsKey(k)) m.put(k, 1);
             else m.put(k, m.get(k) + 1);
         }
-        
         int v = s.nextInt();
-        if (m.containsKey(v))
-            System.out.print(m.get(v));
-        else System.out.print(0);
+        
+        System.out.print(m.getOrDefault(v, 0));
     }
 }
