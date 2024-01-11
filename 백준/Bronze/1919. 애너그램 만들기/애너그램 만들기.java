96b50155b0085e95a95aa1d206cbc2b0614601ce
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-11 15:13:15 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 196 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -2,32 +2,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        String[] arr1 = s.nextLine().split("");
-        String[] arr2 = s.nextLine().split("");
-        Map<String, Integer> m1 = new HashMap<>();
-        Map<String, Integer> m2 = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+
+        for (int k = 0; k < s1.length(); k++)
+            arr1[s1.charAt(k) - 97]++;
+        for (int k = 0; k < s2.length(); k++)
+            arr2[s2.charAt(k) - 97]++;
         
-        Arrays.stream(arr1).forEach(str -> {
-            if (!m1.containsKey(str))
-                m1.put(str, 1);
-            else m1.put(str, m1.get(str) + 1);
-        });
-        Arrays.stream(arr2).forEach(str -> {
-            if (!m2.containsKey(str))
-                m2.put(str, 1);
-            else m2.put(str, m2.get(str) + 1);
-        });
-        
-        int cnt = 0;
-        for (String k : m1.keySet()) {
-            if (m2.containsKey(k)) 
-                cnt += Math.abs(m1.get(k) - m2.get(k));
-            else cnt += m1.get(k);
-        }
-        for (String k : m2.keySet()) 
-            if (!m1.containsKey(k))
-                cnt += m2.get(k);
-        System.out.print(cnt);
+        int sum = 0;
+        for (int k = 0; k < arr1.length; k++)
+            sum += Math.abs(arr1[k] - arr2[k]);
+        System.out.print(sum);
     }
 }
