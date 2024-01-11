46462acb332ab55435759857410000247abd9397
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-11 20:30:17 +0900

    [Silver IV] Title: 스택, Time: 324 ms, Memory: 17592 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,36 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-     public static void main(String[] args) throws IOException {
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-         Stack<Integer> s = new Stack<>();
-         int n = Integer.parseInt(br.readLine());
-         
-         for (int i = 0; i < n; i++) {
-             String[] str = br.readLine().split(" ");
-             switch (str[0]) {
-                 case "push":
-                     s.push(Integer.parseInt(str[1]));
-                     break;
-                 case "pop":
-                     if (!s.isEmpty())
-                         System.out.println(s.pop());
-                     else System.out.println(-1);
-                     break;
-                 case "size":
-                     System.out.println(s.size());
-                     break;
-                 case "empty":
-                     if (s.isEmpty())
-                         System.out.println(1);
-                     else System.out.println(0);
-                     break;
-                 case "top":
-                     if (!s.isEmpty())
-                         System.out.println(s.peek());
-                     else System.out.println(-1);
-                     break;
-             }
-         }
-     }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> s = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            if (str.startsWith("push")) {
+                s.push(Integer.parseInt(str.substring(5)));
+            } else if (str.equals("pop")) {
+                if (s.isEmpty()) System.out.println(-1);
+                else System.out.println(s.pop());
+            } else if (str.equals("size")) {
+                System.out.println(s.size());
+            } else if (str.equals("empty")) {
+                if (s.isEmpty()) System.out.println(1);
+                else System.out.println(0);
+            } else {
+                if (s.isEmpty()) System.out.println(-1);
+                else System.out.println(s.peek());
+            }
+        }
+    }
 }
