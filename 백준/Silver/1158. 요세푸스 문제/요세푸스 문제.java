75a76c046c25ffc943f054f236bf884f5a5fb2e3
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-11 17:27:30 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 524 ms, Memory: 20664 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -2,34 +2,28 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        List<Integer> l = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> l = new LinkedList<>();
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        for (int i = 1; i <= n; i++) l.add(i);
+        
+        Iterator<Integer> it = l.iterator();
         List<Integer> list = new ArrayList<>();
-        int n = s.nextInt();
-        int k = s.nextInt();
-        for (int i = 1; i <= n; i++)
-            l.add(i);
-        
-        int index = k - 1;
-        list.add(l.remove(index));
-        
-        while (l.size() > 0) {
-            if (index + k - 1 >= l.size()) {
-                index--;
-                for (int i = 0; i < k; i++) {
-                    index++;
-                    if (index == l.size())
-                        index = 0;
-                }
+        while (l.size() > 1) {
+            int cnt = k;
+            int tmp = 0;
+            while (cnt > 0) {
+                if (!it.hasNext()) it = l.iterator();
+                int t = it.next();
+                cnt--;
+                if (cnt == 0) tmp = t;
             }
-            else index += k - 1;
-
-            list.add(l.remove(index));
+            list.add(tmp);
+            it.remove();
         }
-
         System.out.print("<");
-        for (int i = 0; i < list.size() - 1; i++)
-            System.out.print(list.get(i) + ", ");
-        System.out.print(list.get(n - 1) +  ">");
+        for (int i : list) System.out.print(i + ", ");
+        System.out.print(l.peek() + ">");
     }
 }
