5d48ba1f10c15a42d2a78031398ed5e0367930e9
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-11 15:07:40 +0900

    [Bronze II] Title: Strfry, Time: 604 ms, Memory: 30364 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -2,31 +2,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = Integer.parseInt(s.nextLine());
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < n; i++) {
-            String[] arr = s.nextLine().split(" ");
+            String[] s = sc.nextLine().split(" ");
+            int[] arr1 = new int[26];
+            int[] arr2 = new int[26];
             
-            if (arr[0].length() != arr[1].length())
-                System.out.println("Impossible");
-            else {
-                if (func(arr[0], arr[1]))
-                    System.out.println("Possible");
-                else System.out.println("Impossible");
-            }  
-        } 
-    }
-    
-    public static boolean func(String s1, String s2) {
-        String[] arr1 = s1.split("");
-        String[] arr2 = s2.split("");
-        
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        
-        for (int i = 0; i < arr1.length; i++) 
-            if (!arr1[i].equals(arr2[i]))
-                return false;
-        return true;
+            for (int k = 0; k < s[0].length(); k++)
+                arr1[s[0].charAt(k) - 97]++;
+            for (int k = 0; k < s[1].length(); k++)
+                arr2[s[1].charAt(k) - 97]++;
+            
+            boolean b = true;
+            for (int k = 0; k < arr1.length; k++)
+                if (arr1[k] != arr2[k]) {
+                    b = false;
+                    break;
+                }
+            if (b) System.out.println("Possible");
+            else System.out.println("Impossible");
+        }
     }
 }
