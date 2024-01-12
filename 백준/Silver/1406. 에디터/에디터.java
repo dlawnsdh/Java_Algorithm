48980c0d711b9089d5516a99ba4676adea76c134
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-12 22:12:27 +0900

    [Silver II] Title: 에디터, Time: 560 ms, Memory: 67180 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -4,44 +4,28 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	String str = br.readLine();
-	int n = Integer.parseInt(br.readLine());
-
-	Stack<Character> l = new Stack<>();
+        Stack<Character> l = new Stack<>();
         Stack<Character> r = new Stack<>();
-        for (int i = 0; i < str.length(); i++)
-            l.push(str.charAt(i));
-		
-	for(int i = 0; i < n; i++) {
-	    String o = br.readLine();
-	    char c = o.charAt(0);
-			
-	    switch(c) {
-		case 'L':
-		    if (!l.isEmpty())
-                    	r.push(l.pop());
-                    break;
-		case 'D':
-		    if(!r.isEmpty())
-			l.push(r.pop());
-		    break;
-		case 'B':
-		    if(!l.isEmpty()) 
-			l.pop();
-		    break;
-	       	case 'P':
-		    l.push(o.charAt(2));
-		    break;
-	    }
-	}
+        String s = br.readLine();
+        int n  = Integer.parseInt(br.readLine());
         
-	while(!l.isEmpty())
-	    r.push(l.pop());
-	while(!r.isEmpty())
-	    bw.write(r.pop());
-		
-	bw.flush();
-	bw.close(); 
+        for (int i = 0; i < s.length(); i++)
+            l.push(s.charAt(i));
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            if (str.equals("L")) {
+                if (!l.isEmpty()) r.push(l.pop());
+            } else if (str.equals("D")) {
+                if (!r.isEmpty()) l.push(r.pop());
+            } else if (str.equals("B")) {
+                if (!l.isEmpty()) l.pop();
+            } else l.push(str.charAt(2));
+        }
+        
+        StringBuilder b = new StringBuilder();
+        while(!l.isEmpty()) b.append(l.pop());
+        b.reverse();
+        while(!r.isEmpty()) b.append(r.pop());
+        System.out.print(b);
     }
 }
