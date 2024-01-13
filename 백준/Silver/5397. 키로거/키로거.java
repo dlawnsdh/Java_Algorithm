ad2ee74bac420824b709f6525ac39d9dbee22d60
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-13 14:27:31 +0900

    [Silver II] Title: 키로거, Time: 1136 ms, Memory: 191576 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -4,39 +4,28 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder b = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
+        int t = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < t; i++) {
+            String s = br.readLine();
             Stack<Character> l = new Stack<>();
             Stack<Character> r = new Stack<>();
-            String str = br.readLine(); 
-
-            for (int k = 0; k < str.length(); k++)
-                switch (str.charAt(k)) {
-                    case '<':
-                        if (!l.isEmpty())
-                            r.push(l.pop());
-                        break;
-                    case '>':
-                        if (!r.isEmpty())
-                            l.push(r.pop());
-                        break;
-                    case '-':
-                        if (!l.isEmpty())
-                            l.pop();
-                        break;
-                    default:
-                        l.push(str.charAt(k));
-                        break;
-                }
-
-            while(!l.isEmpty())
-			    r.push(l.pop());
-		    while(!r.isEmpty())
-			    b.append(r.pop());
-            b.append("\n");
+            
+            for (int k = 0; k < s.length(); k++) {
+                if (s.charAt(k) == '<') {
+                    if (!l.isEmpty()) r.push(l.pop());
+                } else if (s.charAt(k) == '>') {
+                    if (!r.isEmpty()) l.push(r.pop());
+                } else if (s.charAt(k) == '-') {
+                    if (!l.isEmpty()) l.pop();
+                } else l.push(s.charAt(k));
+            }
+            
+            StringBuilder b = new StringBuilder();
+            while (!l.isEmpty()) b.append(l.pop());
+            b.reverse();
+            while (!r.isEmpty()) b.append(r.pop());
+            System.out.println(b);
         }
-        System.out.println(b.toString());
     }
 }
