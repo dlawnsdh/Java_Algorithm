fa49fe1750362a6c47c2dceccf8aef23fd5eb92e
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-16 22:17:28 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int max = 0;
+    
+    public int solution(int k, int[][] dungeons) {
+        dfs(k,  new int[dungeons.length], dungeons, 0);
+        return max;
+    }
+    
+    public void dfs(int k, int[] visited, int[][] dungeons, int sum) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (visited[i] == 0 && k >= dungeons[i][0]) {
+                visited[i] = 1;
+                dfs(k - dungeons[i][1], visited, dungeons, sum + 1);
+                visited[i] = 0;
+            }
+        }
+        if (sum > max) max = sum;
+    }
+}
