2253488c2166598b30ff303677bc0b7e13c0f43d
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-16 14:11:39 +0900

    Update 스킬트리.java

--- 프로그래머스/Lv.2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/Lv.2/49993. 스킬트리/스킬트리.java
@@ -1,6 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = -1;
-        return answer;
+        return (int) Arrays.stream(skill_trees).filter(skill_tree -> {
+            int[] arr = new int[26];
+            for (int i = 0; i < skill_tree.length(); i++) {
+                char c = skill_tree.charAt(i);
+                if (skill.contains(String.valueOf(c))) {
+                    if (skill.startsWith(String.valueOf(c))) 
+                        arr[c - 65]++;
+                    else if (arr[skill.charAt(skill.indexOf(c) - 1) - 65] == 0)
+                        return false;
+                    else arr[c - 65]++;
+                }
+            }
+            return true;
+        }).count();
     }
 }
