ccdfec5711affd16f43ff1bce939931ffbd1453f
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-17 14:45:22 +0900

    [Silver IV] Title: 좋은 단어, Time: 524 ms, Memory: 28048 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -2,26 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-
-        int n = Integer.parseInt(s.nextLine());
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             Stack<Character> st = new Stack<>();
-            String str = s.nextLine();
-            if (str.length() % 2 == 0) {
-                for (int k = 0; k < str.length(); k++) {
-                    if (st.isEmpty())
-                        st.push(str.charAt(k));
-                    else if (st.peek() == str.charAt(k))
-                        st.pop();
-                    else st.push(str.charAt(k));
-                }
-                if (st.isEmpty())
-                    cnt++;
+            String s = sc.nextLine();
+            
+            for (int k = 0; k < s.length(); k++) {
+                if (!st.isEmpty() && st.peek() == s.charAt(k)) st.pop();
+                else st.push(s.charAt(k));
             }
+            
+            if (st.isEmpty()) cnt++; 
         }
-
-        System.out.println(cnt);
+        System.out.print(cnt);
     }
 }
