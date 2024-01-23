c3c3dfab9e324d08b84babf45d77d68481f4c996
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-23 16:47:53 +0900

    [Silver IV] Title: 동전 0, Time: 120 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,22 +1,21 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int k = s.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++)
-            arr[i] = s.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nk = br.readLine().split(" ");
+        int[] arr = new int[Integer.parseInt(nk[0])];
+        for (int i = arr.length - 1; i > -1; i--)
+            arr[i] = Integer.parseInt(br.readLine());
         
         int cnt = 0;
-        for (int i = n - 1; i > -1; i--) {
-            cnt += k / arr[i];
-            k %= arr[i];
-            if (k == 0)
-                break;
+        int idx = 0;
+        int k = Integer.parseInt(nk[1]);
+        while (k > 0) {
+            cnt += k / arr[idx];
+            k %= arr[idx++];
         }
-        
         System.out.print(cnt);
     }
 }
