7819395a383c66e081e77524ce1fcf085a6373d4
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-24 16:45:21 +0900

    [Silver I] Title: 숨바꼭질, Time: 264 ms, Memory: 22104 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -3,27 +3,35 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Queue<Integer> q = new LinkedList<>();
-        int n = sc.nextInt(); int k = sc.nextInt();
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        if (n == k) {
+            System.out.print(0);
+            return;
+        }
+        
         int[] dist = new int[100001];
-        boolean[] visited = new boolean[100001];
         int[] dx = {-1, 1};
+        LinkedList<Integer> q = new LinkedList<>();
+        Arrays.fill(dist, -1);
         
         q.add(n);
-        visited[n] = true;
+        dist[n] = 0;
         while (!q.isEmpty()) {
-            int x = q.peek();
-            q.poll();
+            int x = q.poll();
             for (int i = 0; i < 3; i++) {
                 int nx;
-                if (i != 2) nx = x + dx[i];
-                else nx = 2 * x;
-                if (nx < 0 || nx >= dist.length || visited[nx]) continue;
+                if (i == 2) nx = x * 2;
+                else nx = x + dx[i];
+                if (nx < 0 || nx > 100000) continue;
+                if (dist[nx] >= 0) continue;
                 dist[nx] = dist[x] + 1;
-                visited[nx] = true;
                 q.add(nx);
+                if (nx == k) {
+                    System.out.print(dist[nx]);
+                    return;
+                }
             }
         }
-        System.out.print(dist[k]);
     }
 }
