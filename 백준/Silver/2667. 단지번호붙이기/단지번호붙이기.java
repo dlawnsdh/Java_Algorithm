3063da1428ab2fd0303735605ed5e4529dcacc10
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-25 16:44:24 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,47 +1,45 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Queue<Integer[]> q = new LinkedList<>();
-        int n = Integer.parseInt(sc.nextLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[][] map = new int[n][n];
         boolean[][] visited = new boolean[n][n];
-        int[] dx = {0, 0, 1, -1};
-        int[] dy = {1, -1, 0, 0};
         for (int i = 0; i < n; i++) {
-            String s = sc.nextLine();
+            String s = br.readLine();
             for (int k = 0; k < n; k++)
                 map[i][k] = s.charAt(k) - 48;
         }
         
         int cnt = 0;
-        List<Integer> l =  new ArrayList<>();
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {1, -1, 0, 0};
+        LinkedList<int[]> q = new LinkedList<>();
+        List<Integer> l = new ArrayList<>();
         for (int i = 0; i < n; i++) 
-            for (int k = 0; k < n; k++)
-                if (map[i][k] == 1 && !visited[i][k]) {
-                    q.add(new Integer[] {i, k});
-                    int sum = 0;
+            for (int k = 0; k < n; k++) 
+                if (!visited[i][k] && map[i][k] == 1) {
+                    cnt++;
+                    visited[i][k] = true;
+                    q.add(new int[] {i, k});
                     while (!q.isEmpty()) {
-                        int x = q.peek()[0];
-                        int y = q.peek()[1];
-                        q.poll();
+                        int[] cur = q.poll();
                         for (int j = 0; j < 4; j++) {
-                            int nx = x + dx[j];
-                            int ny = y + dy[j];
+                            int nx = cur[0] + dx[j];
+                            int ny = cur[1] + dy[j];
                             if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                            if (map[nx][ny] == 0 || visited[nx][ny]) continue;
-                            q.add(new Integer[] {nx, ny});
+                            if (visited[nx][ny] || map[nx][ny] == 0) continue;
                             visited[nx][ny] = true;
-                            sum++;
+                            map[i][k]++;
+                            q.add(new int[] {nx, ny});
                         }
                     }
-                    if (sum == 0) sum = 1;
-                    l.add(sum);
-                    cnt++;
+                    l.add(map[i][k]);
                 }
-        System.out.println(cnt);
         Collections.sort(l);
+        System.out.println(cnt);
         l.forEach(System.out::println);
     }
 }
