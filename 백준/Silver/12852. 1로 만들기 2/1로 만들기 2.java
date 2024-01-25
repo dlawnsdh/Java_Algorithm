ab9aa2b5ee276c43d8ed1494bb4118df175e2298
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-25 20:33:41 +0900

    [Silver I] Title: 1로 만들기 2, Time: 152 ms, Memory: 22172 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,31 +1,31 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] d = new int[n + 1];
-        int[] p = new int[n + 1];
-
-        for (int i = 2; i <= n; i++) {
-            d[i] = d[i - 1] + 1;
-            p[i] = i - 1;
-            if (i % 2 == 0) {
-                if (d[i / 2] + 1 < d[i])
-                    p[i] = i / 2;
-                d[i] = Math.min(d[i / 2] + 1, d[i]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] route = new int[n + 1];
+        
+        for (int i = 2; i < n + 1; i++) {
+            arr[i] = arr[i - 1] + 1;
+            route[i] = i - 1;
+            if (i % 2 == 0 && arr[i] > arr[i / 2] + 1) {
+                arr[i] = arr[i / 2] + 1;
+                route[i] = i / 2;   
             }
-            if (i % 3 == 0) {
-                if (d[i / 3] + 1 < d[i])
-                    p[i] = i / 3;
-                d[i] = Math.min(d[i / 3] + 1, d[i]);
+            if (i % 3 == 0 && arr[i] > arr[i / 3] + 1) {
+                arr[i] = arr[i / 3] + 1; 
+                route[i] = i / 3;   
             }
         }
-        System.out.println(d[n]);
-        int index = n;
-        while (index > 0) {
-            System.out.print(index + " ");
-            index = p[index];
+        
+        System.out.println(arr[n]);
+        StringBuilder b = new StringBuilder();
+        while (n > 0) {
+            b.append(n).append(" ");
+            n = route[n];
         }
+        System.out.print(b);
     }
 }
