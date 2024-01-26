41a84d04a95869ed5d9f8c7c8c8084d53933e8b4
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-26 13:35:02 +0900

    [Silver I] Title: 안전 영역, Time: 456 ms, Memory: 58312 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,46 +1,48 @@
 import java.util.*;
+import java.io.*;
+
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Queue<Integer[]> q = new LinkedList<>();
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());    
         int[][] map = new int[n][n];
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
-        int max = 0;
-        for (int i = 0; i < n; i++)
+        Set<Integer> s = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            String[] str = br.readLine().split(" ");
             for (int k = 0; k < n; k++) {
-                map[i][k] = sc.nextInt();
-                max = Math.max(max, map[i][k]);
+                map[i][k] = Integer.parseInt(str[k]);
+                s.add(Integer.parseInt(str[k]));
             }
-        
-        int cnt = 0;
-        for (int h = 0; h < max; h++) {
-            boolean[][] visited = new boolean[n][n];
+        }
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+       
+        List<Integer> l = new ArrayList<>();
+        s.forEach(h -> {
             int sum = 0;
+            LinkedList<int[]> q = new LinkedList<>();
+            boolean[][] visited = new boolean[n][n];
             for (int i = 0; i < n; i++)
                 for (int k = 0; k < n; k++)
-                    if (map[i][k] > h && !visited[i][k]) {
+                    if (!visited[i][k] && map[i][k] > h) {
+                        sum++;
                         visited[i][k] = true;
-                        q.add(new Integer[] {i, k});
+                        q.add(new int[] {i, k});
                         while (!q.isEmpty()) {
-                            int x = q.peek()[0];
-                            int y = q.peek()[1];
-                            q.poll();
+                            int cur[] = q.poll();
                             for (int j = 0; j < 4; j++) {
-                                int nx = x + dx[j];
-                                int ny = y + dy[j];
+                                int nx = cur[0] + dx[j];
+                                int ny = cur[1] + dy[j];
                                 if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                                if (map[nx][ny] <= h || visited[nx][ny]) continue;
+                                if (visited[nx][ny] || map[nx][ny] <= h) continue;
                                 visited[nx][ny] = true;
-                                q.add(new Integer[] {nx, ny});
+                                q.add(new int[] {nx, ny});
                             }
                         }
-                        sum++;
+                        l.add(sum);
                     }
-            cnt = Math.max(cnt, sum);
-        }
-        System.out.print(cnt);
+        });
+        System.out.print(l.stream().max(Integer::compareTo).orElse(1));
     }
 }
