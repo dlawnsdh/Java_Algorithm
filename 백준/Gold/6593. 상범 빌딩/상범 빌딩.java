2ed9de9fcfe6b689e603c2c1df4e8e5595439ace
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-26 14:10:06 +0900

    [Gold V] Title: 상범 빌딩, Time: 200 ms, Memory: 20140 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -4,53 +4,58 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());    
+        int l = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
         int[] dx = {1, -1, 0, 0, 0, 0};
         int[] dy = {0, 0, 1, -1, 0, 0};
         int[] dz = {0, 0, 0, 0, 1, -1};
+        int[] end = new int[3];
         
-        while (true) {
-            String[] arr = br.readLine().split(" ");
-            int l = Integer.parseInt(arr[0]); 
-            int r = Integer.parseInt(arr[1]); 
-            int c = Integer.parseInt(arr[2]);
-            if (l == 0 && r == 0 && c == 0) return;
-            
-            char[][][] map = new char[l][r][c];
+        while (l != 0 && r != 0 && c != 0) {
+            int[][][] map = new int[l][r][c];
             int[][][] dist = new int[l][r][c];
-            Queue<Integer[]> q = new LinkedList<>();
-            int[] goal = new int[3];
+            LinkedList<int[]> q = new LinkedList<>();
             for (int i = 0; i < l; i++) {
                 for (int k = 0; k < r; k++) {
-                    String str = br.readLine();
+                    String s = br.readLine();
                     for (int j = 0; j < c; j++) {
-                        map[i][k][j] = str.charAt(j);
-                        if (map[i][k][j] == 'S')
-                            q.add(new Integer[] {i, k, j});
-                        if (map[i][k][j] == 'E')
-                            goal = new int[] {i, k, j};
+                        map[i][k][j] = s.charAt(j);
+                        dist[i][k][j] = -1;
+                        if (map[i][k][j] == 'S') {
+                            q.add(new int[] {i, k, j});
+                            dist[i][k][j] = 0;
+                        } else if (map[i][k][j] == 'E')
+                            end = new int[] {i, k, j};
                     }
                 }
                 br.readLine();
             }
             
+            boolean flag = true;
             while (!q.isEmpty()) {
-                int x = q.peek()[1];
-                int y = q.peek()[2];
-                int z = q.peek()[0];
-                q.poll();
+                int[] cur = q.poll();
+                if (cur[0] == end[0] && cur[1] == end[1] && cur[2] == end[2]) {
+                    System.out.printf("Escaped in %d minute(s).\n", dist[cur[0]][cur[1]][cur[2]]);
+                    flag = false;
+                    break;
+                }
                 for (int i = 0; i < 6; i++) {
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
-                    int nz = z + dz[i];
+                    int nz = cur[0] + dz[i];
+                    int nx = cur[1] + dx[i];
+                    int ny = cur[2] + dy[i];
                     if (nx < 0 || nx >= r || ny < 0 || ny >= c || nz < 0 || nz >= l) continue;
-                    if (dist[nz][nx][ny] > 0 || map[nz][nx][ny] == '#') continue;
-                    dist[nz][nx][ny] = dist[z][x][y] + 1;
-                    q.add(new Integer[] {nz, nx, ny});
+                    if (dist[nz][nx][ny] >= 0 || map[nz][nx][ny] == '#') continue;
+                    dist[nz][nx][ny] = dist[cur[0]][cur[1]][cur[2]] + 1;
+                    q.add(new int[] {nz, nx, ny});
                 }
             }
-            if (dist[goal[0]][goal[1]][goal[2]] != 0)
-                System.out.println("Escaped in " + dist[goal[0]][goal[1]][goal[2]] + " minute(s).");
-            else System.out.println("Trapped!");
+            if (flag) System.out.println("Trapped!");
+            st = new StringTokenizer(br.readLine());
+            l = Integer.parseInt(st.nextToken());
+            r = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
         }
     }
 }
