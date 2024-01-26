fbf2e5903ffcea2bcf5b34e8121e59b15a570757
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-01-26 12:54:58 +0900

    [Silver I] Title: 스타트링크, Time: 224 ms, Memory: 58108 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,34 +1,34 @@
 import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Queue<Integer> q = new LinkedList<>();
-        int f = sc.nextInt(); int s = sc.nextInt(); int g = sc.nextInt();
-        int u = sc.nextInt(); int d = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int f = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int g = Integer.parseInt(st.nextToken());
+        int[] ud = {Integer.parseInt(st.nextToken()), -Integer.parseInt(st.nextToken())};
         int[] dist = new int[f + 1];
-        boolean[] visited = new boolean[f + 1];
-        int[] dx = {u, -1 * d};
+        Arrays.fill(dist, -1);
         
-        if (s == g) {
-            System.out.print(0);
-            return;
-        }
-        
+        LinkedList<Integer> q = new LinkedList<>();
+        dist[s] = 0;
         q.add(s);
-        visited[s] = true;
         while (!q.isEmpty()) {
-            int x = q.peek();
-            q.poll();
-            for (int i = 0; i < 2; i++) {
-                int nx = x + dx[i];
-                if (nx < 1 || nx > f || visited[nx]) continue;
-                dist[nx] = dist[x] + 1;
+            int cur = q.poll();
+            if (cur == g) {
+                System.out.println(dist[cur]);
+                return;
+            }
+            for (int i : ud) {
+                int nx = cur + i;
+                if (nx < 1 || nx > f) continue;
+                if (dist[nx] >= 0) continue;
+                dist[nx] = dist[cur] + 1;
                 q.add(nx);
-                visited[nx] = true;
             }
         }
-        
-        if (visited[g]) System.out.print(dist[g]);
-        else System.out.print("use the stairs");
+        System.out.println("use the stairs");
     }
 }
