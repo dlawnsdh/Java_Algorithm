08c90ee9b3c585d1bed5932c8c3847e529a3a315
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-02-06 16:39:24 +0900

    [Silver I] Title: 정수 삼각형, Time: 316 ms, Memory: 26704 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,19 +1,32 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        int[][] d = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int k = 0; k < i + 1; k++)
-                arr[i][k] = sc.nextInt();
-        System.arraycopy(arr[n - 1], 0, d[n - 1], 0, n);
-
-        for (int i = n - 2; i > - 1; i--)
-            for (int k = 0; k < i + 1; k++)
-                d[i][k] += arr[i][k] + Math.max(d[i + 1][k], d[i + 1][k + 1]);
-        System.out.print(d[0][0]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<int[]> l = new ArrayList<>();
+        List<int[]> dp = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] arr = new int[i];
+            for (int k = 0; k < i; k++)
+                arr[k] = Integer.parseInt(st.nextToken());
+            l.add(arr);
+            dp.add(new int[i]);
+        }
+        
+        dp.set(0, l.get(0));
+        for (int i = 0; i < n - 1; i++) {
+            int[] arr1 = dp.get(i);
+            int[] arr2 = dp.get(i + 1);
+            for (int k = 0; k < arr1.length; k++) {
+                arr2[k] = Math.max(arr2[k], arr1[k] + l.get(i + 1)[k]);
+                arr2[k + 1] = Math.max(arr2[k + 1], arr1[k] + l.get(i + 1)[k + 1]);
+            }                          
+            dp.set(i + 1, arr2);
+        }
+        Arrays.sort(dp.get(n - 1));
+        System.out.print(dp.get(n - 1)[n - 1]);
     }
 }
