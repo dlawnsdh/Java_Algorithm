c531fed93b8b8265f0002084abe1a9b52d0eeffd
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-02-06 16:48:38 +0900

    [Silver III] Title: 이친수, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,19 +1,15 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        long[] arr = new long[n + 1];
-
-        if (n == 1 || n == 2) 
-            System.out.println(1);
-        else {
-            arr[1] = 1;
-            arr[2] = 1;
-            for (int i = 3; i <= n; i++)
-                arr[i] = arr[i - 1] + arr[i - 2];
-            System.out.println(arr[n]);    
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[91];
+        
+        dp[1] = 1; dp[2] = 1;
+        for (int i = 3; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        System.out.print(dp[n]);
     }
 }
