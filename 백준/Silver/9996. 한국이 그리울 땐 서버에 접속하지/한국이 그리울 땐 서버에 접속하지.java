1a87c86ab5f88eb2ce90c670d9662e9460166aa9
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-02-08 22:03:00 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -1,27 +1,20 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = Integer.parseInt(s.nextLine());
-        String p = s.nextLine();
-        String[] arr = new String[2];
-        for (int i = 0; i < p.length(); i++)
-            if (p.charAt(i) == '*') {
-                arr[0] = p.substring(0, i);
-                arr[1] = p.substring(i + 1);
-            }
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        String[] pattern = br.readLine().split("\\*");
         for (int i = 0; i < n; i++) {
-            String str = s.nextLine();
-
-            if ((arr[0] + arr[1]).equals(str))
-                System.out.println("DA");
-            else {
-                if (arr[0].length() + arr[1].length() < str.length() && arr[0].equals(str.substring(0, arr[0].length())) && arr[1].equals(str.substring(str.length() - arr[1].length())))
-                    System.out.println("DA");
-                else System.out.println("NE");    
+            String s = br.readLine();
+            if (s.length() < pattern[0].length() + pattern[1].length()) {
+                System.out.println("NE");
+                continue;
             }
+            if (s.startsWith(pattern[0]) && s.endsWith(pattern[1]))
+                System.out.println("DA");
+            else System.out.println("NE");
         }
     }
 }
