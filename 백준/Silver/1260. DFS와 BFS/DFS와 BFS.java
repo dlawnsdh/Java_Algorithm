91035ddd4290eea48c05abd9db22f3fa87d8d214
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-02-12 18:48:31 +0900

    [Silver II] Title: DFS와 BFS, Time: 360 ms, Memory: 21140 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,49 +1,56 @@
 import java.util.*;
-public class Main {
-    public static boolean[] visited = new boolean[1001];
-    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+import java.io.*;
 
-    static void dfs(int x) {
-        visited[x] = true;
+public class Main {
+    public static List<List<Integer>> g = new ArrayList<>();
+    
+    public static void dfs(int x, boolean[] visited) {
         System.out.print(x + " ");
-        for (int i = 0; i < graph.get(x).size(); i++)
-            if (!visited[graph.get(x).get(i)])
-                dfs(graph.get(x).get(i));
+        visited[x] = true;
+        for (int i = 0; i < g.get(x).size(); i++) {
+            int y = g.get(x).get(i);
+            if (!visited[y])
+                dfs(y, visited);
+        }
     }
-    static void bfs(int x) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(x);
-        visited[x] = true;
-        System.out.print(x + " ");
-        while(!q.isEmpty()) {
-            int k = q.poll();
-            for (int i = 0; i < graph.get(k).size(); i++) {
-                int y = graph.get(k).get(i);
-                if (!visited[y]) {
-                    System.out.print(y + " ");
-                    visited[y] = true;
-                    q.add(y);
+    
+    public static void bfs(int start, boolean[] visited) {
+        LinkedList<Integer> q = new LinkedList<>();
+        visited[start] = true;
+        q.add(start);
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            System.out.print(cur + " ");
+            for (int i = 0; i < g.get(cur).size(); i++) {
+                int x = g.get(cur).get(i);
+                if (!visited[x]) {
+                    visited[x] = true;
+                    q.add(x);
                 }
             }
         }
     }
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt(); int m = s.nextInt(); int v = s.nextInt();
-
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
         for (int i = 0; i <= n; i++)
-            graph.add(new ArrayList<Integer>());
+            g.add(new ArrayList<>());
         for (int i = 0; i < m; i++) {
-            int x = s.nextInt(); int y = s.nextInt();
-            graph.get(x).add(y);
-            graph.get(y).add(x);
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            g.get(x).add(y);
+            g.get(y).add(x);
         }
-        for (int i = 0; i < graph.size(); i++)
-            Collections.sort(graph.get(i));
-
-        dfs(v);
-        Arrays.fill(visited, false);
+        for (int i = 1; i <= n; i++)
+            Collections.sort(g.get(i));
+        
+        dfs(start, new boolean[1001]);
         System.out.println();
-        bfs(v);
+        bfs(start, new boolean[1001]);
     }
 }
