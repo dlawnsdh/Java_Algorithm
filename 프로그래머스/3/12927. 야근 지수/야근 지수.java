ada18efd2349e7ab434aedb2111120de3891f399
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-02-14 14:16:53 +0900

    [level 3] Title: 야근 지수, Time: 4.67 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        Arrays.sort(works);
+        int sum = Arrays.stream(works).sum() - n;
+        int idx1 = works.length; 
+        int idx2 = 0; 
+        long result = 0;
+        
+        if (sum <= 0) return result;
+        else {
+            while (idx1 > 0) {
+                int t = sum / idx1 > works[idx2] ? works[idx2] : sum / idx1;
+                result += t * t;
+                sum -= t;
+                idx1--;
+                idx2++;
+            }
+            return result;
+        }
+    }
+}
