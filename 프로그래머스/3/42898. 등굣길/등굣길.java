f464770e8777348f1d114ba461965457de63ebf4
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-02-19 16:31:23 +0900

    [level 3] Title: 등굣길, Time: 0.43 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] map = new int[n + 2][m + 2];
+        if (puddles[0].length != 0) {
+            for (int i = 0; i < puddles.length; i++) 
+                map[puddles[i][1]][puddles[i][0]] = 1;
+        }
+      
+        int[][] dp = new int[n + 2][m + 2]; 
+        dp[0][1] = 1;
+        for (int i = 1; i <= n; i++)
+            for (int k = 1; k <= m; k++) {
+                if (map[i][k] == 0)
+                    dp[i][k] = (dp[i][k - 1] + dp[i - 1][k]) % 1000000007;
+            }
+        return dp[n][m];
+    }
+}
