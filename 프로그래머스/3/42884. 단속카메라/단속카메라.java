d1cbf841ad15f3d919d3809e0af063078b981a98
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-02-20 15:11:31 +0900

    [level 3] Title: 단속카메라, Time: 12.09 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,22 +1,16 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
-    public int[] tmp = {-30001, -30001};
-    public int cnt = 0;
-    
     public int solution(int[][] routes) {
-        Arrays.stream(routes)
-            .sorted((arr1, arr2) -> { 
-                if (arr1[1] == arr2[1]) 
-                    return Integer.compare(arr1[0], arr2[0]);
-                return Integer.compare(arr1[1], arr2[1]);
-            }).forEach(route -> {
-                if (route[0] > tmp[1]) {
-                    cnt++;
-                    tmp = route;
-                }
-            });
+        Arrays.sort(routes, (arr1, arr2) -> Integer.compare(arr1[1], arr2[1]));
+        
+        int[] tmp = routes[0];
+        int cnt = 1;
+        for (int i = 1; i < routes.length; i++)
+            if (routes[i][0] > tmp[1]) {
+                cnt++;
+                tmp = routes[i];
+            }
         return cnt;
     }
 }
