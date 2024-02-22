1891d476b61185fcc2e729147688cd3ffd5017f7
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-02-22 16:32:49 +0900

    [level 2] Title: 미로 탈출, Time: 0.22 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
 
 ### 성능 요약
 
-메모리: 76.5 MB, 시간: 0.23 ms
+메모리: 78.6 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 22일 16:07:15
+2024년 02월 22일 16:32:47
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,47 +1,19 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String[] maps) {
-        char[][] map = new char[maps.length][maps[0].length()];
-        int[][] dist = new int[maps.length][maps[0].length()];
+    public int bfs(char[][] map, int[][] dist, int[] start, char end) {
         LinkedList<int[]> q = new LinkedList<>();
-        for (int i = 0; i < maps.length; i++) {
-            map[i] = maps[i].toCharArray();
-            Arrays.fill(dist[i], -1);
-            for (int k = 0; k < maps[i].length(); k++)
-                if (maps[i].charAt(k) == 'S') {
-                    q.add(new int[] {i, k});
-                    dist[i][k] = 0;
-                }
-        }
-        
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
-        int distL = 0;
+        for (int i = 0; i < dist.length; i++)
+            Arrays.fill(dist[i], -1);
+        
+        q.add(start);
+        dist[start[0]][start[1]] = 0;
         while (!q.isEmpty()) {
             int[] cur = q.poll();
-            if (map[cur[0]][cur[1]] == 'L') {
-                q.clear();
-                q.add(cur);
-                distL = dist[cur[0]][cur[1]];
-                for (int i = 0; i < dist.length; i++)
-                    Arrays.fill(dist[i], -1);
-                dist[cur[0]][cur[1]] = 0;
-                break;
-            }
-            for (int i = 0; i < 4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-                if (nx < 0 || nx >= map.length || ny < 0 || ny >= map[0].length) continue;
-                if (dist[nx][ny] >= 0 || map[nx][ny] == 'X') continue;
-                dist[nx][ny] = dist[cur[0]][cur[1]] + 1;
-                q.add(new int[] {nx, ny});
-            }
-        }
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            if (map[cur[0]][cur[1]] == 'E') 
-                return dist[cur[0]][cur[1]] + distL;
+            if (map[cur[0]][cur[1]] == end)
+                return dist[cur[0]][cur[1]];
             for (int i = 0; i < 4; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
@@ -52,5 +24,28 @@ class Solution {
             }
         }
         return -1;
+    }
+    
+    public int solution(String[] maps) {
+        char[][] map = new char[maps.length][maps[0].length()];
+        int[] start = new int[2];
+        int[] lever = new int[2];
+        for (int i = 0; i < maps.length; i++) {
+            map[i] = maps[i].toCharArray();
+            for (int k = 0; k < maps[i].length(); k++) {
+                if (maps[i].charAt(k) == 'S') {
+                    start[0] = i;
+                    start[1] = k;
+                }
+                if (maps[i].charAt(k) == 'L') {
+                    lever[0] = i;
+                    lever[1] = k;
+                }
+            }
+        }
+        
+        int l = bfs(map, new int[map.length][map[0].length], start, 'L');
+        int e = bfs(map, new int[map.length][map[0].length], lever, 'E');
+        return l == -1 || e == -1 ? -1 : l + e;
     }
 }
