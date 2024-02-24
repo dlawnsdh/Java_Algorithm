de0c23f45ae51e7926ceb2998abbb101b7e37e9c
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-02-24 14:53:04 +0900

    [level 2] Title: 가장 큰 수, Time: 3.14 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,14 +3,15 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         StringBuilder b = new StringBuilder();
-        Arrays.stream(numbers).boxed().sorted((i1, i2) -> {
-            String s1 = Integer.toString(i1) + Integer.toString(i2);
-            String s2 = Integer.toString(i2) + Integer.toString(i1);
-            return -Integer.compare(Integer.parseInt(s1), Integer.parseInt(s2)); 
-        }).forEach(b::append);
+        Arrays.stream(numbers)
+            .boxed()
+            .sorted((i1, i2) -> {
+                String s1 = Integer.toString(i1) + Integer.toString(i2);
+                String s2 = Integer.toString(i2) + Integer.toString(i1);
+                return -Integer.compare(Integer.parseInt(s1), Integer.parseInt(s2)); 
+            })
+            .forEach(b::append);
         
-        return Arrays.stream(numbers)
-            .filter(i -> i == 0)
-            .count() == numbers.length ? "0" : b.toString();
+        return b.charAt(0) == '0' ? "0" : b.toString();
     }
 }
