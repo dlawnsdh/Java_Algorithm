8d941853b370042922ff0fbca9ec6be601e156cc
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-03-17 17:58:49 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 10.57 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int storey) {
+        return dfs(storey, ("0" + Integer.toString(storey)).length() - 1, 0);
+    }
+    
+    public int dfs(int n, int idx, int cnt) {
+        int[] arr = Arrays.stream(("0" + Integer.toString(n)).split(""))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        if (idx == 0)
+            return Math.min(arr[1], 10 - arr[1]) + cnt;
+        
+        int t = (int) Math.pow(10, arr.length - idx);
+        return Math.min(
+            dfs(n - (t / 10 * arr[idx]), idx - 1, cnt + (arr[idx])), 
+            dfs(n + t - (t / 10 * arr[idx]), idx - 1, cnt + (10 - (arr[idx])))
+        );
+    }
+}
