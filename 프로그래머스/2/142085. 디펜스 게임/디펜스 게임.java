bf9a823e166bf640a882bd06ded4ac79712927b5
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-04-08 16:38:29 +0900

    [level 2] Title: 디펜스 게임, Time: 0.69 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,17 +1,15 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n, int k, int[] enemy) {
-        if (k >= enemy.length) 
-            return enemy.length;
-        
+    public int solution(int n, int k, int[] enemy) {        
         PriorityQueue<Integer> q = new PriorityQueue<>();
         int round = 0;
         for (int i = 0; i < enemy.length; i++) {
             q.add(enemy[i]);
             if (k <= i)
                 n -= q.poll();
-            if (n < 0) break;
+            if (n < 0) 
+                return round;
             round++;
         }
         return round;
