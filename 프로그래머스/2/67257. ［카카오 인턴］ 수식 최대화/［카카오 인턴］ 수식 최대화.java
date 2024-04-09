da7c02a19bc2beeb750d372b7640a93dabb37069
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-04-09 14:13:06 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 3.91 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -3,47 +3,36 @@ import java.util.regex.*;
 import java.util.stream.*;
 
 class Solution {
+    String[][] operators = {{"*", "+", "-"}, {"*", "-", "+"}, {"+", "*", "-"}, {"+", "-", "*"}, {"-", "+", "*"}, {"-", "*", "+"}};
+    
     public long solution(String expression) {
-        String[][] op = {{"*", "+", "-"}, {"*", "-", "+"}, {"+", "*", "-"}, {"+", "-", "*"}, {"-", "+", "*"}, {"-", "*", "+"}};
-        long max = 0;
         List<String> list = new ArrayList<>();
         Pattern p = Pattern.compile("\\d+|\\D");
         Matcher m = p.matcher(expression);    
         while (m.find()) 
             list.add(m.group());
 
-        for (String[] o : op) {
-            Stack<String> st = new Stack<>();
-            List<String> l = new ArrayList<>();
-            
-            l = list;
-            for (int i = 0; i < l.size(); i++) {
-                if (!st.isEmpty() && st.peek().equals(o[0]))
-                    st.push(calculate(st.pop(), st.pop(), l.get(i)));
-                else st.push(l.get(i));
-            }
-            
-            l = st.stream().collect(Collectors.toList());
-            st.clear();
-            for (int i = 0; i < l.size(); i++) {
-                if (!st.isEmpty() && st.peek().equals(o[1]))
-                    st.push(calculate(st.pop(), st.pop(), l.get(i)));
-                else st.push(l.get(i));
-            }
-            
-            l = st.stream().collect(Collectors.toList());
-            st.clear();
-            for (int i = 0; i < l.size(); i++) {
-                if (!st.isEmpty() && st.peek().equals(o[2]))
-                    st.push(calculate(st.pop(), st.pop(), l.get(i)));
-                else st.push(l.get(i));
-            }
-            max = Math.max(max, Math.abs(Long.parseLong(st.peek())));
+        long max = 0;
+        for (String[] op : operators) {
+            List<String> l = new ArrayList<>(list);
+            for (String operator : op)
+                l = calculate(l, operator).stream().collect(Collectors.toList());
+            max = Math.max(max, Math.abs(Long.parseLong(l.get(0))));
         }
         return max;
     }
+    
+    public Stack<String> calculate(List<String> l, String operator) {
+        Stack<String> st = new Stack<>();
+        for (int i = 0; i < l.size(); i++) {
+            if (!st.isEmpty() && st.peek().equals(operator))
+                st.push(calculateWithOperator(st.pop(), st.pop(), l.get(i)));
+            else st.push(l.get(i));
+        }
+        return st;
+    }
                             
-    public String calculate(String op, String n1, String n2) {
+    public String calculateWithOperator(String op, String n1, String n2) {
         return switch(op) {
             case "+" -> String.valueOf(Long.parseLong(n1) + Long.parseLong(n2));
             case "-" -> String.valueOf(Long.parseLong(n1) - Long.parseLong(n2));
