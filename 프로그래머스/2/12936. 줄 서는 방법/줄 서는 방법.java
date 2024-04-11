5bb95c5ae2327ce76856679019c46eaf821ba1f1
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-04-11 14:06:32 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long k) {         
+        List<Integer> l = new ArrayList<>();
+        long t = 1;
+        for (int i = 1; i <= n; i++) {
+            l.add(i);
+            t *= i;
+        }
+        
+        int[] arr = new int[n];
+        int index = 0;
+        while (!l.isEmpty()) {
+            t /= n;    
+            n--;
+            
+            int idx = k % t == 0 ? (int) (k / t) : (int) (k / t + 1);
+            long r = k % t;
+            arr[index++] = l.get(idx - 1);
+            l.remove(idx - 1);
+            
+            if (r == 0) break;
+            else k = r;
+        }
+        for (int i = l.size() - 1; i >= 0; i--)
+            arr[index++] = l.get(i);
+        return arr;
+    }
+}
