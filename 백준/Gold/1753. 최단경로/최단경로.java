90c69ffab79a70660152aa6e36f0e11253e34cb5
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-04-17 13:32:44 +0900

    [Gold IV] Title: 최단경로, Time: 1008 ms, Memory: 109128 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,16 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-class Node {
-    int n;
-    int w;
-    
-    public Node(int n, int w) {
-        this.n = n;
-        this.w = w;
-    }
-}
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,40 +8,39 @@ public class Main {
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
         int start = Integer.parseInt(br.readLine());
-        
-        List<List<Node>> g = new ArrayList<>();
-        int[] dist = new int[V + 1];
-        for (int i = 0; i <= V; i++) {
+        List<List<int[]>> g = new ArrayList<>();
+        for (int i = 0; i <= V; i++)
             g.add(new ArrayList<>());
-            dist[i] = Integer.MAX_VALUE;
-        }
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            g.get(u).add(new Node(v, w));
+            g.get(u).add(new int[] {v, w});
         }
         
-        PriorityQueue<Node> q = new PriorityQueue<>((a, b) -> a.w - b.w);
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         boolean[] visited = new boolean[V + 1];
+        int[] dist = new int[V + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[start] = 0;
-        q.add(new Node(start, 0));
+        q.add(new int[] {start, 0});
         while (!q.isEmpty()) {
-            Node cur = q.poll();
-            if (visited[cur.n]) continue;
-            visited[cur.n] = true;
-            for (int i = 0; i < g.get(cur.n).size(); i++) {
-                Node x = g.get(cur.n).get(i);
-                if (dist[x.n] > cur.w + x.w) {
-                    dist[x.n] = cur.w + x.w;
-                    q.add(new Node(x.n, dist[x.n]));
+            int[] cur = q.poll();
+            if (visited[cur[0]]) continue;
+            visited[cur[0]] = true;
+            for (int i = 0; i < g.get(cur[0]).size(); i++) {
+                int[] nx = g.get(cur[0]).get(i);
+                if (dist[nx[0]] > dist[cur[0]] + nx[1]) {
+                    dist[nx[0]] = dist[cur[0]] + nx[1];
+                    q.add(new int[] {nx[0], dist[nx[0]]});
                 }
             }
         }
         
-        for (int i = 1; i < dist.length; i++) {
-            if (dist[i] == Integer.MAX_VALUE) System.out.println("INF");
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == Integer.MAX_VALUE)
+                System.out.println("INF");
             else System.out.println(dist[i]);
         }
     }
