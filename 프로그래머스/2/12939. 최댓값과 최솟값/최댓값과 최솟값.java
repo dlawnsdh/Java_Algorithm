8dfdc5f2ff6a4a2b7414abd4f4f307d90de64a40
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-06-11 13:54:06 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 2.90 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        String[] arr1 = s.split(" ");
-        int[] arr2 = new int[arr1.length];
-        for (int i = 0; i < arr1.length; i++)
-            arr2[i] = Integer.parseInt(arr1[i]);
-        Arrays.sort(arr2);
-        return arr2[0] + " " + arr2[arr2.length - 1];
+        int[] arr = Arrays.stream(s.split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        Arrays.sort(arr);
+        StringBuilder b = new StringBuilder();
+        b.append(arr[0]).append(" ").append(arr[arr.length - 1]);
+        return b.toString();
     }
 }
