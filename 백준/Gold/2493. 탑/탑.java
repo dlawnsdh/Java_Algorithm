7057feebea36d7baa5b5d556910623fc3f36cd16
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-06-14 14:34:40 +0900

    [Gold V] Title: 탑, Time: 696 ms, Memory: 86932 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -4,28 +4,27 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Stack<Integer> s = new Stack<>();
-        Stack<Integer> idx = new Stack<>();
-        int n = Integer.parseInt(br.readLine());
-
-        StringBuilder b = new StringBuilder("0 ");
-        String[] arr = br.readLine().split(" ");
-        s.push(Integer.parseInt(arr[0]));
-        idx.push(1);
-        for (int i = 1; i < n; i++) {
-            int k = Integer.parseInt(arr[i]);
-            while (!s.isEmpty() && s.peek() < k) {
-                s.pop();
-                idx.pop();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        
+        StringBuilder b = new StringBuilder();
+        Stack<int[]> s = new Stack<>();
+        for (int i = 1; i <= n; i++) {
+            if (s.isEmpty()) {
+                s.push(new int[] {arr[i], i});
+                b.append(0).append(' ');
+            } else {
+                while (!s.isEmpty() && s.peek()[0] < arr[i])
+                    s.pop();
+                if (s.isEmpty()) b.append(0).append(' ');
+                else b.append(s.peek()[1]).append(' ');
+                s.push(new int[] {arr[i], i});
             }
-            if (s.isEmpty()) b.append("0 ");
-            else b.append(idx.peek()).append(" ");
-            s.push(k);
-            idx.push(i + 1);
         }
-
-        bw.write(String.valueOf(b));
-        bw.flush();
+        System.out.print(b);
     }
 }
