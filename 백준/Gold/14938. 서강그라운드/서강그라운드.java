61e9178ee758620d2d5f7c513d02fe518f6c4094
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-06-18 16:23:58 +0900

    [Gold IV] Title: 서강그라운드, Time: 136 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -2,7 +2,36 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int INF = 200000000;
+    static List<List<int[]>> g = new ArrayList<>();
+    static int[] arr;
+    static int INF = 100000000;
+    
+    static int dijkstra(int st, int m) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        int[] dist = new int[g.size()];
+        boolean[] visited = new boolean[g.size()];
+        Arrays.fill(dist, INF);
+        
+        dist[st] = 0;
+        pq.add(new int[] {st, 0});
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if (visited[cur[0]]) continue;
+            visited[cur[0]] = true;
+            for (int i = 0; i < g.get(cur[0]).size(); i++) {
+                int[] nx = g.get(cur[0]).get(i);
+                if (dist[nx[0]] > dist[cur[0]] + nx[1]) {
+                    dist[nx[0]] = dist[cur[0]] + nx[1];
+                    pq.add(new int[] {nx[0], dist[nx[0]]});
+                }
+            }
+        }
+        int sum = 0;
+        for (int i = 1; i < dist.length; i++)
+            if (dist[i] <= m)
+                sum += arr[i];
+        return sum;
+    }
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,37 +39,24 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n + 1];
-        int[][] map = new int[n + 1][n + 1];
+        arr = new int[n + 1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++)
             arr[i] = Integer.parseInt(st.nextToken());
-            Arrays.fill(map[i], INF);
-            map[i][i] = 0;
-        }
+        for (int i = 0; i <= n; i++)
+            g.add(new ArrayList<>());
         for (int i = 0; i < r; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            map[a][b] = c;
-            map[b][a] = c;
+            g.get(a).add(new int[] {b, c});
+            g.get(b).add(new int[] {a, c});
         }
         
-        for (int k = 1; k <= n; k++)
-            for (int i = 1; i <= n; i++)
-                for (int j = 1; j <= n; j++)
-                    if (map[i][j] > map[i][k] + map[k][j])
-                        map[i][j] = map[i][k] + map[k][j];
         int max = 0;
-        for (int i = 1; i <= n; i++) {
-            int sum = 0;
-            for (int k = 1; k <= n; k++) {
-                if (map[i][k] <= m)
-                    sum += arr[k];
-            }
-            max = Math.max(max, sum);
-        }
+        for (int i = 1; i <= n; i++) 
+            max = Math.max(max, dijkstra(i, m));
         System.out.print(max);
     }
 }
