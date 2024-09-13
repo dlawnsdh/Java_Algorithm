2be2214df907624782bb3726dfb9137716445436
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-13 14:42:44 +0900

    [Silver V] Title: 숫자 카드, Time: 1200 ms, Memory: 108472 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,23 +1,37 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        Set<Integer> set = new HashSet<>();
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++)
-            set.add(s.nextInt());
-        int m = s.nextInt();
-        int[] arr = new int[m];
-        for (int i = 0; i < m; i++)
-            arr[i] = s.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(br.readLine());
         
+        Arrays.sort(arr);
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            if (set.contains(arr[i]))
-                System.out.print(1 + " ");
-            else
-                System.out.print(0 + " ");
+            int k = Integer.parseInt(st.nextToken());
+            int s = 0;
+            int e = n - 1;
+            boolean flag = false;
+            while (s <= e) {
+                int mid = (s + e) / 2;
+                if (arr[mid] == k) {
+                    flag = true;
+                    break;
+                } else if (arr[mid] > k) 
+                    e = mid - 1;
+                else s = mid + 1;
+            }
+            if (flag) sb.append(1);
+            else sb.append(0);
+            sb.append(' ');
         }
+        System.out.print(sb);
     }
 }
