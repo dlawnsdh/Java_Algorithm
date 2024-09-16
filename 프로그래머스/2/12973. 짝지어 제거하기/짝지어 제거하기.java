f11951188c825b2c7cd49b5f4f70fed02a53d9da
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-16 17:48:54 +0900

    [level 2] Title: 짝지어 제거하기, Time: 61.49 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -4,8 +4,9 @@ class Solution {
     public int solution(String s) {
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (!st.isEmpty() && st.peek() == s.charAt(i)) st.pop();
-            else st.push(s.charAt(i));
+            if (st.isEmpty() || st.peek() != s.charAt(i))
+                st.push(s.charAt(i));
+            else st.pop();
         }
         return st.isEmpty() ? 1 : 0;
     }
