74d77170bd6cf465090f62acc705d710e373221a
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-16 16:43:21 +0900

    [level 2] Title: 올바른 괄호, Time: 28.64 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,13 +4,12 @@ class Solution {
     boolean solution(String s) {
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (st.isEmpty() && s.charAt(i) == ')')
-                return false;
-            if (st.isEmpty() || !(st.peek() == '(' && s.charAt(i) == ')'))
+            if (st.isEmpty() || s.charAt(i) == '(')
                 st.push(s.charAt(i));
-            else st.pop();
+            else if (st.peek() == '(' && s.charAt(i) == ')')
+                st.pop();
+            else st.push(s.charAt(i));
         }
-        
         return st.isEmpty() ? true : false;
     }
 }
