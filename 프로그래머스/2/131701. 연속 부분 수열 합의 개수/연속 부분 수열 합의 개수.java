31957f45ee3d3c89e3aa73787d3a6db9102321e0
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-20 15:31:00 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1485.46 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -2,16 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        Set<Integer> s = new HashSet<>();
-        
-        for (int i = 0; i < elements.length; i++) {
-            int sum = elements[i];
-            s.add(sum);
-            for (int k = i + 1; k < elements.length + i; k++) { 
-                sum += elements[k % elements.length];
-                s.add(sum);
+        Set<Integer> count = new HashSet<>();
+        for (int i = 0; i < elements.length; i++)
+            for (int k = 1; k < elements.length; k++) {
+                int sum = 0;
+                for (int j = 0; j < k; j++)
+                    sum += elements[(i + j) % elements.length];
+                count.add(sum);
             }
-        }
-        return s.size();
+        return count.size() + 1;
     }
 }
