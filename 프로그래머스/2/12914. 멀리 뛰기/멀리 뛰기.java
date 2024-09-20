dba2b8ded8dbecba8a33b339b55065cf177c27f9
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-20 13:55:10 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,10 +1,10 @@
 class Solution {
     public long solution(int n) {
-        long[] dist = new long[2001];
-        dist[1] = 1;
-        dist[2] = 2;
-        for (int i = 3; i <= n; i++)
-            dist[i] = (dist[i - 1] + dist[i - 2]) % 1234567;
-        return dist[n];
+        long[] dp = new long[2001];
+        dp[1] = 1; 
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) 
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        return dp[n];
     }
 }
