31c1dadec4bca7a77905fb5bdee3f4d76794f7f3
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-20 14:46:40 +0900

    [level 2] Title: 예상 대진표, Time: 0.10 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,16 +1,11 @@
 class Solution {
     public int solution(int n, int a, int b) {
-        int cnt = 1;
-        while (n > 1) {
-            if (Math.max(a, b) % 2 == 0 && Math.abs(a - b) == 1) 
-                break;
-            if (a % 2 == 0) a /= 2;
-            else a = (a + 1) / 2;
-            if (b % 2 == 0) b /= 2;
-            else b = (b + 1) / 2;
-            cnt++;
-            n /= 2;
+        int round = 1;
+        while (Math.abs(a - b) > 1 || Math.max(a, b) % 2 != 0) {
+            a = a % 2 == 0 ? a / 2 : a / 2 + 1;
+            b = b % 2 == 0 ? b / 2 : b / 2 + 1;
+            round++;
         }
-        return cnt;
+        return round;
     }
 }
