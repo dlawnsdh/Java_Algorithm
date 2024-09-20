92552057dc3189b11d18517f1acff82fd168337b
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-20 14:31:18 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,21 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        Set<String> s = new HashSet<>();
-        int[] arr = new int[n + 1];
-        
-        int cnt = 1;
-        s.add(words[0]);
-        arr[1]++;
+        Set<String> word = new HashSet<>();
+        int[] result = new int[2];
+        word.add(words[0]);
+        int num = 2;
+        int turn = 1;
         for (int i = 1; i < words.length; i++) {
-            cnt++;
-            if (s.contains(words[i]) || words[i].charAt(0) != words[i - 1].charAt(words[i - 1].length() - 1)) {
-                return new int[] {cnt, arr[cnt] + 1};
+            if (!word.contains(words[i]) && words[i - 1].endsWith(String.valueOf(words[i].charAt(0)))) {
+                word.add(words[i]);
+                num++;   
+            } else {
+                result[0] = num;
+                result[1] = turn;
+                break;
             }
-            s.add(words[i]);
-            arr[cnt]++;
-            if (cnt == n) cnt = 0;
+            if (num > n) {
+                num = 1;
+                turn++;
+            }
         }
-        return new int[] {0, 0};
+        return result;
     }
 }
