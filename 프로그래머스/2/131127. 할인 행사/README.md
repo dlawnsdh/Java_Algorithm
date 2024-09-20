556b3093f2be48dbd18cf91fff0079f499c65dbf
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-20 16:20:06 +0900

    [level 2] Title: 할인 행사, Time: 45.71 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 77.2 MB, 시간: 0.02 ms
+메모리: 109 MB, 시간: 45.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 13:33:5
+2024년 09월 20일 16:20:04
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,19 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        int cnt = 0;
-        for (int i = 0; i <= discount.length - 10; i++) {
-            Map<String, Integer> m = new HashMap<>();
-            for (int k = i; k < i + 10; k++)
-                m.put(discount[k], m.getOrDefault(discount[k], 0) + 1);
-            
-            boolean flag = true;
-            for (int k = 0; k < want.length; k++) {
-                if (!m.containsKey(want[k]) || m.get(want[k]) != number[k])
-                    flag = false;
+        Map<String, Integer> m = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < discount.length; i++) {
+            if (i < 10) 
+                m.compute(discount[i], (k, v) -> v == null ? 1 : v + 1);
+            else {
+                m.compute(discount[i], (k, v) -> v == null ? 1 : v + 1);
+                m.compute(discount[i - 10], (k, v) -> v - 1);
+                if (m.get(discount[i - 10]) == 0)
+                    m.remove(discount[i - 10]);
             }
-            if (flag) cnt++;
+            if (i >= 9) {
+                boolean flag = true;
+                for (int k = 0; k < want.length; k++)
+                    if (!m.containsKey(want[k]) || m.get(want[k]) != number[k]) {
+                        flag = false;
+                        break;
+                    }
+                if (flag) count++;
+            }
         }
-        return cnt;
+        return count;
     }
 }
