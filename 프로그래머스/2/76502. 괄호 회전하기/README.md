f81dab8af99e5b30101f1fc45ea8a67ebe25b242
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-20 16:03:00 +0900

    [level 2] Title: 괄호 회전하기, Time: 83.59 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 0.46 ms
+메모리: 81.5 MB, 시간: 83.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 13:51:5
+2024년 09월 20일 16:02:57
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,28 +1,24 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String s) {
-        int n = 0;
-        int cnt = 0;
-        LinkedList<Character> q = new LinkedList<>();
-        for (int i = 0; i < s.length(); i++)
-            q.add(s.charAt(i));
-        
-        while (n < s.length()) {
-            Stack<Character> st = new Stack<>();
-            for (int i = 0; i < q.size(); i++) {
-                if (q.get(i) == '(' || q.get(i) == '{' || q.get(i) == '[')
-                    st.push(q.get(i));
-                else {
-                    if (!st.isEmpty() && ((st.peek() == '(' && q.get(i) == ')') || (st.peek() == '{' && q.get(i) == '}') || (st.peek() == '[' && q.get(i) == ']')))
-                        st.pop();
-                    else st.push(q.get(i));
-                }
-            }
-            if (st.isEmpty()) cnt++;
-            q.add(q.poll());
-            n++;
+    boolean isCorrectBracket(int start, String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = start; i < s.length() + start; i++) {
+            int idx = i % s.length();
+            if (st.isEmpty())
+                st.push(s.charAt(idx));
+            else if ((st.peek() == '(' && s.charAt(idx) == ')') || (st.peek() == '{' && s.charAt(idx) == '}') || (st.peek() == '[' && s.charAt(idx) == ']'))
+                st.pop();
+            else st.push(s.charAt(idx));
         }
-        return cnt;
+        return st.isEmpty() ? true : false;
+    }
+    
+    public int solution(String s) {        
+        int count = 0;
+        for (int i = 0; i < s.length(); i++)
+            if (isCorrectBracket(i, s)) 
+                count++;
+        return count;
     }
 }
