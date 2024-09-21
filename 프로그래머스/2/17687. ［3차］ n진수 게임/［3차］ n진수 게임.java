b472a4ff683bd61a3de116a5adc0416c21e9629d
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-21 14:44:16 +0900

    [level 2] Title: [3차] n진수 게임, Time: 13.28 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,31 +1,13 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        char[] arr = new char[m * t];
-        int idx = 0;
-        int num = 0;
-        while (idx < arr.length) {
-            StringBuilder b = new StringBuilder(); 
-            int k = num++;
-            while (k >= n) {
-                if (k % n >= 10)
-                    b.append((char) (k % n + 55));
-                else b.append(k % n);
-                k /= n;
-            }
-            if (k >= 10) b.append((char) (k + 55));
-            else b.append(k);
-            b.reverse();
-            
-            for (int i = 0; i < b.length(); i++) {
-                arr[idx++] = b.charAt(i);
-                if (idx >= arr.length) 
-                    break;
-            }
-        }
-        
         StringBuilder sb = new StringBuilder();
-        for (int i = p - 1; i < arr.length; i += m)
-            sb.append(arr[i]);
-        return sb.toString();
+        int num = 0;
+        while (sb.length() < t * m)
+            sb.append(Integer.toString(num++, n).toUpperCase());
+        
+        StringBuilder b = new StringBuilder();
+        for (int i = p - 1; i < t * m; i += m)
+            b.append(sb.charAt(i));
+        return b.toString();
     }
 }
