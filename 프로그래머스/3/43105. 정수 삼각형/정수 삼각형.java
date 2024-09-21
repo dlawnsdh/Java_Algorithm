a884f18718a614af50233ce86279c1d754e0ab69
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-21 16:47:26 +0900

    [level 3] Title: 정수 삼각형, Time: 10.09 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,20 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] triangle) {
-        List<int[]> dp = new ArrayList<>();
-        for (int i = 1; i <= triangle.length; i++)
-            dp.add(new int[i]);
+        int[][] dp = new int[triangle.length + 1][triangle.length + 2]; // 양 쪽에 패딩을 위해 2칸 추가
+        dp[1][1] = triangle[0][0];
+        int max = 0;
+        for (int i = 2; i <= triangle.length; i++)
+            for (int k = 1; k <= i; k++)
+                dp[i][k] = Math.max(dp[i - 1][k - 1], dp[i - 1][k]) + triangle[i - 1][k - 1];
         
-        dp.set(0, triangle[0]);
-        for (int i = 0; i < triangle.length - 1; i++) {
-            int[] arr1 = dp.get(i);
-            int[] arr2 = dp.get(i + 1);
-            for (int k = 0; k < arr1.length; k++) {
-                arr2[k] = Math.max(arr2[k], arr1[k] + triangle[i + 1][k]);
-                arr2[k + 1] = Math.max(arr2[k + 1], arr1[k] + triangle[i + 1][k + 1]);
-            }
-        }
-        Arrays.sort(dp.get(triangle.length - 1));
-        return dp.get(triangle.length - 1)[triangle.length - 1];
+        Arrays.sort(dp[triangle.length]);
+        return dp[triangle.length][triangle.length + 1];
     }
 }
