c8087c9f6a0b975632844720cede1454488eceac
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-21 13:52:19 +0900

    [level 2] Title: 타겟 넘버, Time: 4.20 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] numbers, int target) {
-        return dfs(numbers, 0, target, 0);
-    }
-    
-    public int dfs(int[] numbers, int idx, int target, int sum) {
+    int dfs(int target, int[] numbers, int idx, int sum) {
         if (idx == numbers.length) {
             if (sum == target) return 1;
             else return 0;
         }
-        
-        return dfs(numbers, idx + 1, target, sum + numbers[idx]) + dfs(numbers, idx + 1, target, sum - numbers[idx]);
+        return dfs(target, numbers, idx + 1, sum + numbers[idx]) 
+            + dfs(target, numbers, idx + 1, sum - numbers[idx]);
+    }
+    
+    public int solution(int[] numbers, int target) {
+        return dfs(target, numbers, 0, 0);
     }
 }
