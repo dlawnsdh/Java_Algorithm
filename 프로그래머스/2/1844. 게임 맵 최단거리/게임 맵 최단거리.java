1b84f158042437bb43d8d7736e670c52e908ea35
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-22 13:16:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.23 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] maps) {
-        int[][] dist = new int[maps.length][maps[0].length];
-        LinkedList<int[]> q = new LinkedList<>();
+    int bfs(int[][] map, int n, int m) {
+        Queue<int[]> q = new LinkedList<>();
+        int[][] dist = new int[n][m];
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
-        
-        for (int i = 0; i < maps.length; i++)
+        for (int i = 0; i < n; i++)
             Arrays.fill(dist[i], -1);
+        
         dist[0][0] = 1;
         q.add(new int[] {0, 0});
         while (!q.isEmpty()) {
@@ -16,12 +16,16 @@ class Solution {
             for (int i = 0; i < 4; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
-                if (nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length) continue;
-                if (dist[nx][ny] >= 0 || maps[nx][ny] == 0) continue;
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (dist[nx][ny] >= 0 || map[nx][ny] == 0) continue;
                 dist[nx][ny] = dist[cur[0]][cur[1]] + 1;
                 q.add(new int[] {nx, ny});
             }
         }
-        return dist[maps.length - 1][maps[0].length - 1];
+        return dist[n - 1][m - 1];
+    }
+    
+    public int solution(int[][] maps) {
+        return bfs(maps, maps.length, maps[0].length);
     }
 }
