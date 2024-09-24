045842b170ff851871628c7a50a4839393f3ecbf
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-24 16:23:47 +0900

    [level 2] Title: 모음 사전, Time: 7.25 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,38 +1,21 @@
+import java.util.*;
+
 class Solution {
-    public int solution(String word) {
-        char[] arr = {'A', 'E', 'I', 'O', 'U'};
-        int cnt = 1;
-        StringBuilder sb = new StringBuilder();
-        for (int a = 0; a < 5; a++) {
-            sb.append(arr[a]);
-            if (sb.toString().equals(word)) return cnt;
-            else cnt++;
-            for (int b = 0; b < 5; b++) {
-                sb.append(arr[b]);
-                if (sb.toString().equals(word)) return cnt;
-                else cnt++;
-                for (int c = 0; c < 5; c++) {
-                    sb.append(arr[c]);
-                    if (sb.toString().equals(word)) return cnt;
-                    else cnt++;
-                    for (int d = 0; d < 5; d++) {
-                        sb.append(arr[d]);
-                        if (sb.toString().equals(word)) return cnt;
-                        else cnt++;
-                        for (int e = 0; e < 5; e++) {
-                            sb.append(arr[e]);
-                            if (sb.toString().equals(word)) return cnt;
-                            else cnt++;
-                            sb.deleteCharAt(4);
-                        }
-                        sb.delete(3, 4);
-                    }
-                    sb.delete(2, 4);
-                }
-                sb.delete(1, 4);
-            }
-            sb.delete(0, 4);
+    String[] arr = {"A", "E", "I", "O", "U"}; 
+    Map<String, Integer> m = new HashMap<>();
+    int count = 1;
+    
+    void dfs(String word, int depth) {
+        if (depth == 5)
+            return;
+        for (int i = 0; i < 5; i++) {
+            m.put(word + arr[i], count++);
+            dfs(word + arr[i], depth + 1);
         }
-        return cnt;
+    }
+    
+    public int solution(String word) {
+        dfs("", 0);
+        return m.get(word);
     }
 }
