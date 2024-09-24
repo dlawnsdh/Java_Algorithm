d57c8f688bf33c768fda5a2d3e9748ab122d9395
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-24 17:01:33 +0900

    [level 3] Title: 야근 지수, Time: 127.69 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,20 +2,17 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] works) {
-        Arrays.sort(works);
-        int sum = Arrays.stream(works).sum() - n;
-        int idx1 = works.length; 
-        int idx2 = 0; 
-        long result = 0;
-        
-        if (sum <= 0) return result;
-        while (idx1 > 0) {
-            int t = sum / idx1 > works[idx2] ? works[idx2] : sum / idx1;
-            result += t * t;
-            sum -= t;
-            idx1--;
-            idx2++;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int w : works)
+            pq.add(w);
+        while (n-- > 0 && !pq.isEmpty()) {
+            if (pq.peek() > 1)
+                pq.add(pq.poll() - 1);
+            else pq.poll();
         }
-        return result;
+        long sum = 0;
+        while (!pq.isEmpty())
+            sum += Math.pow(pq.poll(), 2);
+        return sum;
     }
 }
