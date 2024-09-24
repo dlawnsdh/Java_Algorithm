abef7e2c562bbf4c65011bac0af8da28d75661ab
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-24 18:18:47 +0900

    [level 2] Title: 방문 길이, Time: 20.77 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(String dirs) {
-        Set<String> visited = new HashSet<>();
-        int[] from = {0, 0};
-        int[][] dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
-            
-        for (int i = 0; i < dirs.length(); i++) {
-            int[] to = switch (dirs.charAt(i)) {
-                case 'U' -> new int[] {from[0], from[1] + 1};
-                case 'D' -> new int[] {from[0], from[1] - 1};
-                case 'L' -> new int[] {from[0] - 1, from[1]};
-                case 'R' -> new int[] {from[0] + 1, from[1]};   
-                default -> new int[] {from[0], from[1]};
-            };
-            if (to[0] < -5 || to[0] > 5 || to[1] < -5 || to[1] > 5) 
-                continue;
-            String fromTo = String.format("%d%d%d%d", from[0], from[1], to[0], to[1]); 
-            String toFrom = String.format("%d%d%d%d", to[0], to[1], from[0], from[1]); 
-            if (!visited.contains(fromTo) && !visited.contains(toFrom))
-                visited.add(fromTo);
-            from = new int[] {to[0], to[1]};
+        Set<String> s = new HashSet<>();
+        int x = 0;
+        int y = 0;
+        for (char op : dirs.toCharArray()) {
+            int nx = x;
+            int ny = y;
+            if (op == 'U') ny++;
+            else if (op == 'D') ny--;
+            else if (op == 'R') nx++;
+            else nx--;
+            if (nx < -5 || nx > 5 || ny < -5 || ny > 5) continue;
+            String route = String.format("%d%d%d%d", x, y, nx, ny);
+            String reverse = String.format("%d%d%d%d", nx, ny, x, y);
+            s.add(route);
+            s.add(reverse);
+            x = nx;
+            y = ny;
         }
-        return visited.size();
+        return s.size() / 2;
     }
 }
