8bae263d5f11e22bcd8aeb64b8f91ef52d4b957f
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-25 13:51:37 +0900

    [level 3] Title: 단어 변환, Time: 0.68 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,37 +1,62 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String begin, String target, String[] words) {        
+    class Word {
+        String word;
+        int idx;
+        
+        Word(String w, int i) {
+            this.word = w;
+            this.idx = i;
+        }
+    }
+    
+    boolean isOneAlphabetChanged(String from, String to) {
+        int cnt = 0;
+        for (int i = 0; i < from.length(); i++)
+            if (from.charAt(i) != to.charAt(i))
+                cnt++;
+        return cnt == 1 ? true : false;
+    }
+    
+    int min = Integer.MAX_VALUE;
+    void dfs(String[] words, boolean[] visited, String begin, String target, int count) {
+        if (begin.equals(target)) {
+            min = Math.min(min, count);
+            return;
+        } else {
+            for (int i = 0; i < words.length; i++)
+                if (!visited[i]) {
+                    visited[i] = true;
+                    dfs(words, visited, words[i], target, count + 1);
+                    visited[i] = false;
+                }
+        }
+    } 
+    
+    int bfs(String[] words, String begin, String target) {
+        Queue<Word> q = new LinkedList<>();
         int[] dist = new int[words.length + 1];
-        LinkedList<String> q1 = new LinkedList<>();
-        LinkedList<Integer> q2 = new LinkedList<>();
-        List<String> l = new ArrayList<>(Arrays.asList(words));
-        l.add(0, begin);
         
         Arrays.fill(dist, -1);
-        q1.add(begin);
-        q2.add(0);
         dist[0] = 0;
-        while (!q1.isEmpty()) {
-            String cur = q1.poll();
-            int idx = q2.poll();
-            if (cur.equals(target))
-                return dist[l.indexOf(target)];
-            
-            for (int i = 1; i < l.size(); i++) {
-                String next = l.get(i);
-                if (dist[i] > 0) continue;
-                int cnt = 0;
-                for (int k = 0; k < next.length(); k++)
-                    if (next.charAt(k) != cur.charAt(k))
-                        cnt++;
-                if (cnt == 1) {
-                    dist[i] = dist[idx] + 1;
-                    q1.add(next);
-                    q2.add(i);
+        q.add(new Word(begin, 0));
+        while (!q.isEmpty()) {
+            Word cur = q.poll();
+            if (cur.word.equals(target))
+                return dist[cur.idx];
+            for (int i = 0; i < words.length; i++)
+                if (isOneAlphabetChanged(cur.word, words[i]) && dist[i + 1] < 0) {
+                    dist[i + 1] = dist[cur.idx] + 1;
+                    q.add(new Word(words[i], i + 1));
                 }
-            }
         }
         return 0;
+    }
+    
+    public int solution(String begin, String target, String[] words) {        
+        return bfs(words, begin, target);
+        // dfs(words, new boolean[words.length], begin, target, 0);
+        // return min;
     }
 }
