161e6a7a2458ecd5e01d007ebd4448a304905a01
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-25 13:24:34 +0900

    [level 2] Title: 땅따먹기, Time: 19.65 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -2,17 +2,19 @@ import java.util.*;
 
 class Solution {
     int solution(int[][] land) {
-        for (int i = 1; i < land.length; i++) {
-            for (int k = 0; k < 4; k++) {
-                int tmp = land[i][k];
-                for (int j = 0; j < 4; j++)
-                    if (j != k)
-                        tmp = Math.max(tmp, land[i][k] + land[i - 1][j]);
-                land[i][k] = tmp;
-            }
-        }
+        int[][] dp = new int[land.length][4];
+        for (int i = 0; i < 4; i++)
+            dp[0][i] = land[0][i];
         
-        Arrays.sort(land[land.length - 1]);
-        return land[land.length - 1][3];
+        for (int i = 1; i < dp.length; i++)
+            for (int k = 0; k < 4; k++) {
+                int max = 0;
+                for (int j = 0; j < 4; j++) {
+                    if (j == k) continue;
+                    max = Math.max(max, dp[i - 1][j]);
+                }
+                dp[i][k] = land[i][k] + max;
+            }
+        return Arrays.stream(dp[dp.length - 1]).max().getAsInt();
     }
 }
