a51c4e54cb9d874c894f3a0ac68b15742f3b42cb
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-27 17:17:11 +0900

    [level 3] Title: 숫자 게임, Time: 134.51 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -2,18 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] A, int[] B) {
-        Arrays.sort(A);
-        Arrays.sort(B);
-        int idxA = 0;
-        int idxB = 0;
-        int cnt = 0;
-        while (idxA < A.length && idxB < B.length) {
-            if (A[idxA] < B[idxB]) {
-                idxA++;
-                idxB++;
-                cnt++;
-            } else idxB++;
+        PriorityQueue<Integer> a = new PriorityQueue<>();
+        PriorityQueue<Integer> b = new PriorityQueue<>();
+        for (int i = 0;  i < A.length; i++) {
+            a.add(A[i]);
+            b.add(B[i]);
         }
-        return cnt;
+        
+        int count = 0;
+        while (!b.isEmpty()) {
+            if (b.peek() > a.peek()) {
+                a.poll();
+                count++;
+            }
+            b.poll();
+        }
+        return count;
     }
 }
