7bfba007a58bc8e50a9c47cfd6790179094ad0ca
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-27 14:40:49 +0900

    [level 2] Title: 주차 요금 계산, Time: 11.14 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,55 +1,53 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    public int[] solution(int[] fees, String[] records) {        
-        Map<String, ArrayList<Fee>> m = new HashMap<>();
-        for (String s : records) {
-            String[] arr = s.split(" ");
-            Fee fee = new Fee(
-                Integer.parseInt(arr[1]),
-                Integer.parseInt(arr[0].substring(0, 2)),
-                Integer.parseInt(arr[0].substring(3)),
-                arr[2]
-            );
-            ArrayList<Fee> l = m.getOrDefault(arr[1], new ArrayList<>());
-            l.add(fee);
-            m.put(arr[1], l);
+    class Info implements Comparable<Info>{
+        String carNum;
+        List<Integer> record = new ArrayList<>();
+        
+        Info(String n) { 
+            this.carNum = n; 
         }
         
-        return m.values().stream()
-            .sorted((l1, l2) -> Integer.compare(l1.get(0).number, l2.get(0).number))
-            .map(l -> {
-                int sum = 0;
-                if (l.size() == 1) sum = (23 - l.get(0).hour) * 60 + (59 - l.get(0).minute);
-                else if (l.size() % 2 == 0) {
-                    for (int i = 0; i < l.size() - 1; i += 2) {
-                        Fee fee1 = l.get(i);
-                        Fee fee2 = l.get(i + 1);
-                        sum += (fee2.hour - fee1.hour) * 60 + (fee2.minute - fee1.minute);
-                    }    
-                } else {
-                    for (int i = 0; i < l.size() - 2; i += 2) {
-                        Fee fee1 = l.get(i);
-                        Fee fee2 = l.get(i + 1);
-                        sum += (fee2.hour - fee1.hour) * 60 + (fee2.minute - fee1.minute);
-                    }
-                    sum += (23 - l.get(l.size() - 1).hour) * 60 + (59 - l.get(l.size() - 1).minute);
-                }
-                return sum <= fees[0] ? fees[1] : fees[1] + (int) Math.ceil((double) (sum - fees[0]) / (double) fees[2]) * fees[3];
-            }).mapToInt(Integer::intValue).toArray();
+        int getTotalFee(int[] fees) {
+            if (record.size() % 2 != 0)
+                record.add(convertTime("23:59"));
+            
+            int totalTime = 0;
+            for (int i = 1; i < record.size(); i += 2)
+                totalTime += record.get(i) - record.get(i - 1);
+                
+            if (totalTime <= fees[0])
+                return fees[1];
+            return fees[1] + (int) Math.ceil((double) (totalTime - fees[0]) / fees[2]) * fees[3];
+        }
+        
+        @Override
+        public int compareTo(Info o) {
+            return this.carNum.compareTo(o.carNum);
+        }
     }
     
-    class Fee {
-        int number;
-        int hour;
-        int minute;
-        String state;
-        
-        public Fee (int n, int h, int m, String s) {
-            number = n;
-            hour = h;
-            minute = m;
-            state = s;
+    int convertTime(String time) {
+        int hour = Integer.parseInt(time.substring(0, 2));
+        int minute = Integer.parseInt(time.substring(3));
+        return hour * 60 + minute;
+    }
+    
+    Map<String, Info> fee = new HashMap<>();
+    
+    public int[] solution(int[] fees, String[] records) {        
+        for (String r : records) {
+            String carNum = r.substring(6, 10);
+            fee.computeIfAbsent(carNum, k -> new Info(carNum))
+                .record
+                .add(convertTime(r.substring(0, 5)));
         }
+        
+        return fee.values().stream()
+            .sorted()
+            .mapToInt(info -> info.getTotalFee(fees))
+            .toArray();
     }
 }
