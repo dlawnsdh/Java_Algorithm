369ede83fe2b8e5d162f7274ec6394dcc91e4bc1
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-09-29 18:14:11 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 18.39 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,58 +1,55 @@
 import java.util.*;
 
 class Solution {
+    char[][] map;
+    
+    boolean is2x2Block(int x, int y) {
+        if (map[x][y] == 'X')
+            return false;
+        if (map[x][y] == map[x + 1][y] && map[x][y] == map[x][y + 1] && map[x][y] == map[x + 1][y + 1])
+            return true;
+        return false;
+    }
+    
     public int solution(int m, int n, String[] board) {
-        LinkedList<int[]> q = new LinkedList<>();
-        char[][] map = new char[m][n];
+        map = new char[m][n];
         for (int i = 0; i < board.length; i++)
             map[i] = board[i].toCharArray();
         
-        boolean isDeleted = false;
-        int total = 0;
+        int count = 0;
         while (true) {
+            Queue<int[]> q = new LinkedList<>();
+            boolean isRemoved = false;
             for (int i = 0; i < m - 1; i++)
                 for (int k = 0; k < n - 1; k++)
-                    if (isBlock(map, i, k) && !isZero(map, i, k)) {
+                    if (is2x2Block(i, k)) {
                         q.add(new int[] {i, k});
                         q.add(new int[] {i + 1, k});
                         q.add(new int[] {i, k + 1});
                         q.add(new int[] {i + 1, k + 1});
-                        isDeleted = true;
+                        isRemoved = true;
                     }
-            if (!isDeleted) break;
-            isDeleted = false;
+            if (!isRemoved) break;
             
             while (!q.isEmpty()) {
-                int[] xy = q.poll();
-                if (map[xy[0]][xy[1]] != '0') {
-                    map[xy[0]][xy[1]] = '0';
-                    total++;
+                if (map[q.peek()[0]][q.peek()[1]] != 'X') {
+                    map[q.peek()[0]][q.peek()[1]] = 'X';
+                    count++;
                 }
+                q.poll();
             }
             
             for (int i = 0; i < n; i++) {
-                StringBuilder b = new StringBuilder();
-                int cnt = 0;
-                for (int k = m - 1; k >= 0; k--) {
-                    if (map[k][i] != '0') 
-                        b.append(map[k][i]);
-                    else cnt++;
-                }
-                for (int j = 0; j < cnt; j++)
-                    b.append(0);
-                for (int k = m - 1; k >= 0; k--)
-                    map[k][i] = b.charAt(m - k - 1);
+                StringBuilder sb = new StringBuilder();
+                for (int k = 0; k < m; k++)
+                    if (map[k][i] != 'X')
+                        sb.append(map[k][i]);
+                for (int k = 0; k < m - sb.length(); k++)
+                    map[k][i] = 'X';
+                for (int k = m - sb.length(); k < m; k++)
+                    map[k][i] = sb.charAt(k - (m - sb.length()));
             }
-            
         }
-        return total;
-    }
-    
-    public boolean isZero(char[][] map, int x, int y) {
-        return map[x][y] == '0' || map[x + 1][y] == '0' || map[x][y + 1] == '0' || map[x + 1][y + 1] == '0';
-    }
-    
-    public boolean isBlock(char[][] map, int x, int y) {
-        return map[x][y] == map[x + 1][y] && map[x][y] == map[x][y + 1] && map[x][y] == map[x + 1][y + 1];
+        return count;
     }
 }
