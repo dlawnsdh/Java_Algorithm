db6d0d99063e8d3f27735d430e0997c33ee33c21
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-10-02 16:10:20 +0900

    [level 2] Title: 큰 수 만들기, Time: 77.95 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        Stack<Integer> s = new Stack<>();
-        int cnt = k;
-        for (int i = 0; i < number.length(); i++) {
-            while (!s.isEmpty() && cnt > 0 && s.peek() < number.charAt(i) - 48) {
-                s.pop();
-                cnt--;    
-            }
-            if (s.size() < number.length() - k)
-                s.push(number.charAt(i) - 48);
+        Stack<Integer> st = new Stack<>();
+        int remove = 0;
+        for (char n : number.toCharArray()) {
+            while (!st.isEmpty() && st.peek() < n - 48 && remove++ < k)
+                st.pop();
+            st.push(n - 48);
         }
         
-        StringBuilder b = new StringBuilder();
-        while (!s.isEmpty())
-            b.append(s.pop());
-        return b.reverse().toString();
+        StringBuilder sb = new StringBuilder();
+        while (st.size() > number.length() - k)
+            st.pop();
+        while (!st.isEmpty())
+            sb.append(st.pop());
+        return sb.reverse().toString();
     }
 }
