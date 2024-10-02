5868f3714d15ba557f459c37f07ab55bb09cfff9
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-10-02 13:00:59 +0900

    [level 2] Title: 소수 찾기, Time: 7.43 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,42 +1,35 @@
 import java.util.*;
 
 class Solution {
-    public Set<Integer> s = new HashSet<>();
+    Set<Integer> prime = new HashSet<>();
     
-    public void permutation(int[] arr, int[] result, boolean[] visited, int k, int r) {
-        if (k == r) {
-            StringBuilder b = new StringBuilder();
-            for (int i : result)
-                b.append(i);
-            if (isPrime(Integer.parseInt(b.toString()))) 
-                s.add(Integer.parseInt(b.toString()));
-            return;
-        }
-        
-        for (int i = 0; i < arr.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                result[k] = arr[i];
-                permutation(arr, result, visited, k + 1, r);
-                visited[i] = false;
-            }
-        }
-    }
-    
-    public boolean isPrime(int n) {
+    boolean isPrime(int n) {
         if (n < 2) return false;
         for (int i = 2; i <= Math.sqrt(n); i++)
-            if (n % i == 0)
+            if (n % i == 0) 
                 return false;
         return true;
     }
     
+    void dfs(char[] num, boolean[] visited, char[] arr, int d, int r) {
+        if (d == r) {
+            int i = Integer.parseInt(String.valueOf(arr));
+            if (isPrime(i))
+                prime.add(i);
+        } else {
+            for (int i = 0; i < num.length; i++)
+                if (!visited[i]) {
+                    visited[i] = true;
+                    arr[d] = num[i];
+                    dfs(num, visited, arr, d + 1, r);
+                    visited[i] = false;
+                }
+        }
+    }
+    
     public int solution(String numbers) {
-        int[] arr = new int[numbers.length()];
-        for (int i = 0; i < numbers.length(); i++)
-            arr[i] = numbers.charAt(i) - 48;
-        for (int i = 1; i <= arr.length; i++)
-            permutation(arr, new int[i], new boolean[arr.length], 0, i);
-        return s.size();
+        for (int i = 1; i <= numbers.length(); i++)
+            dfs(numbers.toCharArray(), new boolean[numbers.length()], new char[i], 0, i);
+        return prime.size();
     }
 }
