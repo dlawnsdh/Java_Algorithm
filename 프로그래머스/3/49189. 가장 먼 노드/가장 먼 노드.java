a6c01f80246bf170b542d9d1d08792acd1be2a03
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-10-03 19:20:05 +0900

    [level 3] Title: 가장 먼 노드, Time: 56.07 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -5,12 +5,12 @@ class Solution {
         List<List<Integer>> g = new ArrayList<>();
         for (int i = 0; i <= n; i++)
             g.add(new ArrayList<>());
-        for (int[] arr : edge) {
-            g.get(arr[0]).add(arr[1]);
-            g.get(arr[1]).add(arr[0]);
+        for (int[] e : edge) {
+            g.get(e[0]).add(e[1]);
+            g.get(e[1]).add(e[0]);
         }
         
-        LinkedList<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         int[] dist = new int[n + 1];
         Arrays.fill(dist, -1);
         dist[1] = 0;
@@ -18,14 +18,17 @@ class Solution {
         while (!q.isEmpty()) {
             int cur = q.poll();
             for (int i = 0; i < g.get(cur).size(); i++) {
-                int x = g.get(cur).get(i);
-                if (dist[x] >= 0) continue;
-                dist[x] = dist[cur] + 1;
-                q.add(x);
+                int nx = g.get(cur).get(i);
+                if (dist[nx] < 0) {
+                    dist[nx] = dist[cur] + 1;
+                    q.add(nx);
+                }
             }
         }
         
         Arrays.sort(dist);
-        return (int) Arrays.stream(dist).filter(i -> i == dist[n]).count();
+        return (int) Arrays.stream(dist)
+            .filter(d -> d == dist[n])
+            .count();
     }
 }
