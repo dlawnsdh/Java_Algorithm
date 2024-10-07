1da86ec0ed8c1fb33e172939b2bb020dd5933fe3
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-10-07 17:06:19 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.66 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,43 +1,40 @@
 import java.util.*;
 
 class Solution {
+    int bfs(List<List<Integer>> g) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[g.size()];
+        visited[1] = true;
+        q.add(1);
+        int count = 1;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            for (int i = 0; i < g.get(cur).size(); i++) {
+                int nx = g.get(cur).get(i);
+                if (!visited[nx]) {
+                    visited[nx] = true;
+                    q.add(nx);
+                    count++;
+                }
+            }
+        }
+        int rest = g.size() - count - 1;
+        return Math.abs(rest - count);
+    }
+    
     public int solution(int n, int[][] wires) {
-        int idx = 0;
-        int min = n;
-        while (idx < wires.length) {
+        int min = 100;
+        for (int remove = 0; remove < wires.length; remove++) {
             List<List<Integer>> g = new ArrayList<>();
             for (int i = 0; i <= n; i++)
                 g.add(new ArrayList<>());
-            for (int i = 0; i < wires.length; i++)
-                if (i != idx) {
-                    g.get(wires[i][0]).add(wires[i][1]);
-                    g.get(wires[i][1]).add(wires[i][0]);
-                }
-            
-            LinkedList<Integer> q = new LinkedList<>();
-            boolean[] visited = new boolean[n + 1];
-            List<Integer> count = new ArrayList<>();
-            for (int i = 1; i <= n; i++) {
-                if (!visited[i]) {
-                    int cnt = 1;
-                    q.add(i);
-                    visited[i] = true;
-                    while (!q.isEmpty()) {
-                        int cur = q.poll();
-                        for (int k = 0; k < g.get(cur).size(); k++) {
-                            int x = g.get(cur).get(k);
-                            if (!visited[x]) {
-                                visited[x] = true;
-                                q.add(x);
-                                cnt++;
-                            }
-                        }
-                    }
-                    count.add(cnt);
-                }    
+            for (int i = 0; i < wires.length; i++) {
+                if (i == remove) continue;
+                g.get(wires[i][0]).add(wires[i][1]);
+                g.get(wires[i][1]).add(wires[i][0]);
             }
-            min = Math.min(min, Math.abs(count.get(0) - count.get(1)));
-            idx++;
+            
+            min = Math.min(min, bfs(g));
         }
         return min;
     }
