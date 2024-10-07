98a2b5651761d3ff3ef73c7294ff8c0ce84c52f5
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-10-07 17:50:42 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,28 +1,15 @@
 class Solution {
     public String solution(int n) {
-        StringBuilder b = new StringBuilder();
-        String[] num = {"1", "2", "4"};
-        int numOfDigit = 0;
-        int sum = 0;
-        while (sum < n) {
-            numOfDigit++;
-            sum += Math.pow(3, numOfDigit);
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            if (n % 3 == 0) {
+                sb.append(4);
+                n = n / 3 - 1;
+            } else {
+                sb.append(n % 3);
+                n /= 3;
+            }
         }
-        
-        n -= sum - Math.pow(3, numOfDigit);
-        int idx = numOfDigit - 1;
-        while (idx > 0 && n > 0) {
-            if (n % (int) Math.pow(3, idx) == 0)
-                b.append(num[(n / (int) Math.pow(3, idx) - 1) % 3]);
-            else b.append(num[(n / (int) Math.pow(3, idx)) % 3]);
-            n %= Math.pow(3, idx);
-            idx--;
-        }
-        
-        if (n == 0)
-            for (int i = b.length(); i < numOfDigit; i++)
-                b.append(4);
-        else b.append(num[n - 1]);
-        return b.toString();
+        return sb.reverse().toString();
     }
 }
--- 프로그래머스/2/12899. 124 나라의 숫자/README.md
+++ 프로그래머스/2/12899. 124 나라의 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 124 나라의 숫자 - 12899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899#) 
 
 ### 성능 요약
 
-메모리: 52.1 MB, 시간: 0.05 ms
+메모리: 51.7 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 13일 16:00:13
+2024년 10월 07일 17:50:40
 
 ### 문제 설명
 
