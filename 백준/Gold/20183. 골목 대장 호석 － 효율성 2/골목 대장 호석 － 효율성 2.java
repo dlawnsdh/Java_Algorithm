9559c14cde6a00c026bc887737141a6deeffa96a
JUNO LIM <77203609+dlawnsdh@users.noreply.github.com>
2024-10-21 16:10:20 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 3744 ms, Memory: 339500 KB, Score: 43 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.java
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.java
@@ -6,43 +6,41 @@ public class Main {
     
     static class Node implements Comparable<Node> {
         int next;
-        int cost;
-        long rest;
+        long cost;
         
-        Node(int n, int c, long r) {
+        Node(int n, long c) {
             this.next = n;
             this.cost = c;
-            this.rest = r;
         }
         
         @Override
         public int compareTo(Node o) {
-            return this.cost - o.cost;
+            return Long.compare(this.cost, o.cost);
         }
     } 
     
-    static int dijkstra(int st, int en, long c) {
+    static long dijkstra(int st, int en, int cost) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        int[] dist = new int[g.size()];
-        Arrays.fill(dist, -1);
+        long[] dist = new long[g.size()];
+        Arrays.fill(dist, Long.MAX_VALUE);
         
-        int max = 0;
         dist[st] = 0;
-        pq.add(new Node(st, 0, c));
+        pq.add(new Node(st, 0));
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
-            if (cur.next == en)
-                return max;
+            if (dist[cur.next] < cur.cost)
+                continue;
             for (int i = 0; i < g.get(cur.next).size(); i++) {
                 Node nx = g.get(cur.next).get(i);
-                if (dist[nx.next] < 0 && dist[nx.next] < nx.cost && cur.rest - nx.cost >= 0) {
-                    max = Math.max(max, nx.cost);
-                    dist[nx.next] = nx.cost;
-                    pq.add(new Node(nx.next, dist[nx.next], cur.rest - nx.cost));
+                if (nx.cost > cost)
+                    continue;
+                if (dist[nx.next] > dist[cur.next] + nx.cost) {
+                    dist[nx.next] = dist[cur.next] + nx.cost;
+                    pq.add(new Node(nx.next, dist[nx.next]));
                 }
             }
         }
-        return -1;
+        return dist[en];
     }
     
     public static void main(String[] args) throws IOException {
@@ -60,11 +58,29 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            int z = Integer.parseInt(st.nextToken());
-            g.get(x).add(new Node(y, z, 0));
-            g.get(y).add(new Node(x, z, 0));
+            long z = Long.parseLong(st.nextToken());
+            g.get(x).add(new Node(y, z));
+            g.get(y).add(new Node(x, z));
         }
         
-        System.out.print(dijkstra(a, b, c));
+        // 문제에 최대중에 최소 또는 최소중에 최대를 구하면서 범위가 크다면 매개변수 탐색일 확률이 큼
+        // 수금액을 매개변수 탐색으로 찾고 mid값으로 다익스트라를 실행한다.
+        // 지정한 mid(요금)보타 실제 요금이 큰 경우는 return 값이 Long.MAX_VALUE 이기때문에 
+        // 최대 요금인 c 보다 무조건 크므로 mid 값을 증가시켜서 요금을 낼 수 있게 해야함
+        // 그렇기 때문에 요금의 총합이 c 보다 큰 경우에 mid 를 증가시키고 
+        // 아닌경우에 mid 값을 감소시켜 최대중에 최소를 찾을 수 있게끔 한다.
+        int start = 1;
+        int end = 1000000000;
+        int max = -1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (dijkstra(a, b, mid) > c)
+                start = mid + 1;
+            else {
+                end = mid - 1;
+                max = mid;
+            }
+        }
+        System.out.print(max);
     }
 }
